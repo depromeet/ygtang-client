563fed7d684b9a579900c3c65559576b4e6e9da3
Hyesung Oh <haesungoh414@gmail.com>
2022-04-07 02:40:17 +0900

    env: Framer-motion, SWR, Recoil 설치 및 기본 폴더 구조 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from '@emotion/react';
 import { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
 
 import Theme from '../styles/Theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={Theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={Theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </RecoilRoot>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { css, Theme } from '@emotion/react';
+import { motion } from 'framer-motion';
 
 export default function Root() {
   return (
     <div>
       <h1 css={titleCss}>영감탱</h1>
       <div css={sampleCss}></div>
+      <motion.h2 drag>드래그해보세용</motion.h2>
     </div>
   );
 }
@@ -16,5 +18,6 @@ const titleCss = css`
 const sampleCss = (theme: Theme) => css`
   width: 100px;
   height: 100px;
+  color: white;
   background-color: ${theme.color.black};
 `;
