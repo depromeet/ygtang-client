6903d8b9187573a4778d78d69dd4a81062b0bd28
hyesung oh <haesungoh414@gmail.com>
2022-04-10 17:48:16 +0900

    공용 훅 - 인터널 라우터 개발 (#25)
    * env: axios 설치
    
    * env: baseUrl 설정
    
    * env: lint rule 변경 및 적용
    
    * feat: 공용 훅 - 인터널 라우터 개발
    
    * fix: directory 구조 적용 및 gitkeep 삭제

---
+++ src/hooks/common/useInternalRouter.ts
@@ -0,0 +1,24 @@
+import { useMemo } from 'react';
+import { useRouter } from 'next/router';
+import { UrlObject } from 'url';
+
+type RouterPathType = `/` | '/test';
+
+export default function useInternalRouter() {
+  const router = useRouter();
+
+  return useMemo(() => {
+    return {
+      ...router,
+      push(path: RouterPathType, as?: UrlObject | string, options?: TransitionOptions) {
+        router.push(path, as, options);
+      },
+    };
+  }, [router]);
+}
+
+interface TransitionOptions {
+  shallow?: boolean;
+  locale?: string | false;
+  scroll?: boolean;
+}
