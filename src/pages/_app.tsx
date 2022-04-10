5ea29892e6e639441f9bb2fa613b4b9ff2677136
hyesung oh <haesungoh414@gmail.com>
2022-04-10 17:47:30 +0900

    절대 경로 설정 및 추가 패키지 설치 (#24)
    * env: axios 설치
    
    * env: baseUrl 설정
    
    * env: lint rule 변경 및 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { AppProps } from 'next/app';
 import { ThemeProvider } from '@emotion/react';
 import { RecoilRoot } from 'recoil';
 
-import Theme from '../styles/Theme';
+import Theme from '~/styles/Theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
