d0c65f1f2aa5b1f690f8d898cc674a3f18f03157
Positive Ko <edie.spark1er@gmail.com>
2022-04-14 00:14:10 +0900

    영감 보기 그리드 디자인 설정 및 글로벌 스타일 수정 (#35)
    * feat: home 화면 content view 추가 및 content 썸네일 생성
    
    * design: global styles 전역 적용 및 theme 수정
    
    * design: content thumbnail 디자인 작성 및 공통컴포넌트 tag 추가
    
    * design: global styles reset 추가 및 reset 변경에 따른 디자인 세부 수정
    
    * design: Content View 시맨틱 태그 적용 및 세부 css 프로퍼티 수정
    
    * design: global styles reset 모듈화 및 style 속성 수정

--- src/styles/GlobalStyle/GlobalStyle.tsx
+++ src/styles/GlobalStyle/GlobalStyle.tsx
@@ -1,11 +1,40 @@
-import { css, Global } from '@emotion/react';
+import { css, Global, Theme } from '@emotion/react';
+
+import { resetCss } from './reset';
 
 export default function GlobalStyle() {
   return <Global styles={globalCss} />;
 }
 
-const globalCss = css`
+const globalCss = (theme: Theme) => css`
+  ${resetCss}
+
   * {
     box-sizing: border-box;
+    word-break: keep-all;
+    word-wrap: break-word;
+    -ms-overflow-style: none;
+
+    ::-webkit-scrollbar {
+      display: none !important;
+    }
+  }
+
+  :root {
+    color: ${theme.color.black};
+
+    body {
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+
+      .draggable {
+        -webkit-user-select: all;
+        -moz-user-select: all;
+        -ms-user-select: all;
+        user-select: all;
+      }
+    }
   }
 `;
