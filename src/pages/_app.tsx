b3a81d28b6d595d984d75cca31ec6e262b1d48b7
Hyesung Oh <haesungoh414@gmail.com>
2022-04-14 00:02:14 +0900

    env: 최대 크기 레이아웃 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
+import { PropsWithChildren } from 'react';
 import { AppProps } from 'next/app';
-import { ThemeProvider } from '@emotion/react';
+import { css, ThemeProvider } from '@emotion/react';
 import { RecoilRoot } from 'recoil';
 
 import Theme from '~/styles/Theme';
@@ -8,8 +9,20 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <ThemeProvider theme={Theme}>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </ThemeProvider>
     </RecoilRoot>
   );
 }
+
+function Layout({ children }: PropsWithChildren<{}>) {
+  return <div css={layoutCss}>{children}</div>;
+}
+
+const layoutCss = css`
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+`;
