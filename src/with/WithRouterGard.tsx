46b38d3cde1cdd2044c3ed0add84d6b859f9d05b
정대윤 <sensecodevalue@gmail.com>
2022-04-16 04:05:53 +0900

    feat: next/router의 event를 활용한 라우터 가드 HOC 구현

---
+++ src/with/WithRouterGard.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
+export default function WithRouterGard({ children }: Props): JSX.Element {
+  const router = useRouter();
+  const [authorized, setAuthorized] = useState(false);
+
+  useEffect(() => {
+    authCheck(router.asPath);
+    router.events.on('routeChangeComplete', authCheck);
+
+    return () => {
+      router.events.off('routeChangeStart', hideContent);
+      router.events.off('routeChangeComplete', authCheck);
+    };
+  }, []);
+
+  const hideContent = () => {
+    router.events.on('routeChangeStart', hideContent);
+    setAuthorized(false);
+  };
+
+  const authCheck = (url: string) => {
+    const publicRoutes = ['/password', '/signup', '/login'];
+    const path = url.split('?')[0];
+
+    if (!publicRoutes.includes(path)) {
+      setAuthorized(false);
+      router.push({
+        pathname: '/login',
+        query: { returnUrl: router.asPath },
+      });
+    } else {
+      setAuthorized(true);
+    }
+  };
+
+  return authorized ? children : <></>;
+}
+
+export interface Props {
+  children: JSX.Element;
+}
