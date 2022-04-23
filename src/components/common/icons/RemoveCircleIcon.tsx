e82e661f7a446cb7c637d70e883d5bcc9ce5b4b3
doda <ddark.kr@gmail.com>
2022-04-23 17:53:07 +0900

    design: material icons 추가

---
+++ src/components/common/icons/RemoveCircleIcon.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from 'react';
+
+import Svg, { SvgProps } from '~/components/common/Svg';
+
+export function RemoveCircleIcon({ ...props }: PropsWithChildren<SvgProps>) {
+  return (
+    <Svg viewBox={24} {...props}>
+      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm4 11H8c-.55 0-1-.45-1-1s.45-1 1-1h8c.55 0 1 .45 1 1s-.45 1-1 1z" />
+    </Svg>
+  );
+}
