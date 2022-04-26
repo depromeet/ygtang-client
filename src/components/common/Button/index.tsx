bfcfb593443dbf74bde1822ca82f53995676f784
Hyesung Oh <haesungoh414@gmail.com>
2022-04-27 01:39:26 +0900

    feat: export IconButton

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -2,3 +2,4 @@ export { default } from './Button';
 export { CTAButton } from './CTAButton';
 export { FilledButton } from './FilledButton';
 export { GhostButton } from './GhostButton';
+export { IconButton } from './IconButton';
