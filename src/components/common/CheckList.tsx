f39252b42396a60891f57720bc354c6de73833c8
정대윤 <sensecodevalue@gmail.com>
2022-05-01 19:53:20 +0900

    feat: input을 사용으로 변경 및 클릭 범위 변경

--- src/components/common/CheckList.tsx
+++ src/components/common/CheckList.tsx
@@ -1,31 +1,44 @@
-import { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useEffect, useState } from 'react';
 import { css, useTheme } from '@emotion/react';
 
 import { CheckCircleIcon, ChevronIcon } from './icons';
 
 export interface Props {
+  id: string;
   isChecked: boolean;
-  onToggle: VoidFunction;
-  onClickChevron?: VoidFunction;
+  onToggle: (checked: boolean) => void;
+  onClick?: VoidFunction;
 }
 
 export default function CheckList({
+  id,
   children,
-  isChecked = false,
+  isChecked,
   onToggle,
-  onClickChevron,
+  onClick,
 }: PropsWithChildren<Props>) {
   const theme = useTheme();
 
+  const [checked, setChecked] = useState(isChecked);
+
+  const onCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
+
+  useEffect(() => {
+    onToggle(checked);
+  }, [checked, onToggle]);
+
   return (
     <div css={checkListContainerCss}>
-      <button onClick={onToggle} css={IconWrapperCss}>
-        <CheckCircleIcon color={isChecked ? '' : theme.color.gray01} />
-      </button>
-      <span css={childrenWrapperCss}>{children}</span>
-      <button css={IconWrapperCss} onClick={onClickChevron}>
-        <ChevronIcon css={chevronIconCss} />
-      </button>
+      <input css={inputCheckboxHiddenCss} id={id} type="checkbox" onChange={onCheck} />
+      <label htmlFor={id}>
+        <CheckCircleIcon color={checked ? '' : theme.color.gray01} />
+      </label>
+      <span css={childrenWrapperCss} onClick={onClick}>
+        {children}
+        <ChevronIcon direction="right" />
+      </span>
     </div>
   );
 }
@@ -39,16 +52,22 @@ const checkListContainerCss = css`
 `;
 
 const childrenWrapperCss = css`
-  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
   padding-left: 6px;
+  flex: 1;
   font-size: 12px;
   line-height: 150%;
 `;
 
-const IconWrapperCss = css`
-  line-height: 0;
-`;
-
-const chevronIconCss = css`
-  transform: rotate(180deg);
+const inputCheckboxHiddenCss = css`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  padding: 0;
+  overflow: hidden;
+  border: 0;
+  clip: rect(0 0 0 0);
 `;
