c49ccc2755555f783c0e06782218aee29080b0f4
positiveko <edie.spark1er@gmail.com>
2022-05-05 23:25:10 +0900

    feat: link input 입력 시 link thumbnail로 view 변경

--- src/components/add/LinkInput.tsx
+++ src/components/add/LinkInput.tsx
@@ -1,22 +1,63 @@
+import { useState } from 'react';
 import { css, Theme } from '@emotion/react';
 
 import { PlusIcon } from '~/components/common/icons';
+import useInput from '~/hooks/common/useInput';
+import { useToast } from '~/store/Toast';
 
 import { Input } from '../common/TextField/Input';
+import LinkThumbnail from '../LinkThumbnail';
 
-function PlusBtn() {
+interface OpenGraph {
+  description: string;
+  site_name: string;
+  title: string;
+  url: string;
+  imageUrl?: string;
+}
+
+const TEMP_OG: OpenGraph = {
+  description: '네이버 메인에서 다양한 정보와 유용한 컨텐츠를 만나 보세요',
+  title: '네이버',
+  site_name: 'naver',
+  url: 'https://www.naver.com',
+  imageUrl: 'https://s.pstatic.net/static/www/mobile/edit/2016/0705/mobile_212852414260.png',
+};
+
+function PlusBtn({ onClick }: { onClick: VoidFunction }) {
   return (
-    <button css={btnCss}>
+    <button css={btnCss} onClick={onClick}>
       <PlusIcon color="white" />
     </button>
   );
 }
 
 export default function LinkInput() {
+  const { fireToast } = useToast();
+  const [openGraph, setOpenGraph] = useState<OpenGraph | null>(null);
+  const url = useInput({});
+
+  const showErrorMessage = () => fireToast({ content: '유효하지 않은 주소입니다.' });
+
+  const getOpenGraph = () => {
+    url.value ? setOpenGraph(TEMP_OG) : showErrorMessage();
+  };
+
   return (
     <div css={LinkInputCss}>
-      <Input placeholder="영감이 되는 링크를 붙여 넣어보세요" fixedHeight={32} />
-      <PlusBtn />
+      {openGraph ? (
+        <LinkThumbnail thumbnail={openGraph} edit onDelete={() => setOpenGraph(null)} />
+      ) : (
+        <>
+          <Input
+            placeholder="영감이 되는 링크를 붙여 넣어보세요"
+            fixedHeight={32}
+            value={url.value}
+            onChange={url.onChange}
+          />
+          <PlusBtn onClick={getOpenGraph} />
+        </>
+      )}
     </div>
   );
 }
