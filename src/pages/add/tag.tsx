18259c27fb080a9629720d3d359db789c97d116e
정대윤 <sensecodevalue@gmail.com>
2022-05-06 02:09:04 +0900

    feat: 영감추가 Tag에 TagForm 적용

---
+++ src/pages/add/tag.tsx
@@ -0,0 +1,32 @@
+import { css } from '@emotion/react';
+
+import NavigationBar from '~/components/common/NavigationBar';
+import { TagType } from '~/components/common/Tag';
+import TagFrom from '~/components/TagForm';
+import { useAddTag } from '~/store/AddTag';
+
+export default function AddTag() {
+  const { tags, removeTag, addTag } = useAddTag();
+
+  const tagsB: TagType[] = [
+    { id: 1, content: 'hi hihi hi hi hi hi hi ' },
+    { id: 2, content: 'h2' },
+    { id: 3, content: 'h3' },
+    { id: 4, content: 'h4' },
+    { id: 5, content: 'h5' },
+    { id: 6, content: 'h6' },
+  ];
+
+  return (
+    <article css={addTagCss}>
+      <NavigationBar title="영감 추가" />
+      <TagFrom applyedTags={tags} registeredTags={tagsB} onSave={addTag} onRemove={removeTag} />
+    </article>
+  );
+}
+
+const addTagCss = css`
+  display: flex;
+  flex-direction: column;
+  row-gap: 16px;
+`;
