cd4ed13e46db3b546ff27d1e5d5d7dd52b598969
정대윤 <sensecodevalue@gmail.com>
2022-05-06 02:08:18 +0900

    feat: TagForm 컴포넌트 추가 영감추가 편집에서 사용

---
+++ src/components/TagForm/AppleyedTags.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { css } from '@emotion/react';
+
+import Tag, { TagType } from '~/components/common/Tag';
+
+export default function AppleyedTags({
+  applyedTags = [],
+  onRemove,
+}: {
+  applyedTags: TagType[];
+  onRemove: (id: number) => void;
+}) {
+  // NOTE:  언급은 없었지만 추가할때마다 왼쪽으로 쫙 땡겨지는게 맞지 안나해서 넣었습니다.
+  //        조율 이후 제거 될 수 있습니다.
+  //        focus 사용 배제 이유 아래로 등록된 테그를 스크롤 쭉 내린 상태에서 focus하면 위로 올라오는것 방지하기 위해서 였습니다.
+  // TODO:  스크롤 animation 논의 필요
+  const $container = useRef<HTMLDivElement>(null);
+  const [previousTags, setPreviousTags] = useState<TagType[]>([]);
+  const scrollToLeftMax = () => {
+    if (!$container?.current) return;
+    $container.current.scroll({
+      left: $container.current.scrollWidth,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    if (previousTags.length && previousTags.length < applyedTags.length) {
+      scrollToLeftMax();
+    }
+    setPreviousTags([...applyedTags]);
+  }, [applyedTags]);
+
+  return useMemo(
+    () => (
+      <div ref={$container} css={applyedTagsCss}>
+        {applyedTags.map(tag => (
+          <Tag
+            key={tag.id}
+            tag={tag}
+            deletable
+            onDelete={() => {
+              onRemove(tag.id);
+            }}
+          />
+        ))}
+      </div>
+    ),
+    [applyedTags, onRemove]
+  );
+}
+
+const applyedTagsCss = css`
+  display: flex;
+  padding: 8px 0;
+  column-gap: 6px;
+  overflow-x: scroll;
+`;
---
+++ src/components/TagForm/index.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { css } from '@emotion/react';
+
+import { TagType } from '~/components/common/Tag';
+import { SearchBar } from '~/components/common/TextField';
+import useInput from '~/hooks/common/useInput';
+
+import AppleyedTags from './AppleyedTags';
+import RegisteredTagList from './RegisteredTagList';
+
+export default function TagFrom({
+  applyedTags = [],
+  registeredTags = [],
+  onSave,
+  onRemove,
+}: {
+  applyedTags: TagType[];
+  registeredTags: TagType[];
+  onSave: (tag: TagType) => void;
+  onRemove: (id: number) => void;
+}) {
+  const { value, setValue, onChange } = useInput({ useDebounce: false });
+
+  const onFormReturn = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!value) {
+      return;
+    }
+    // TODO:  search API 추가
+    // NOTE:  핸드폰 UI에서는 안그러겠지만 키보드로 첬을때 엔터 너무 빨리눌러서 Debounce 못쫏아오며
+    //        굳이 Debounce걸어서 서치할 필요도 없기에 사용안하는것으로 결정했습니다.
+    const datetime = new Date().getTime();
+    const searchedTag = { id: datetime, content: value };
+    if (!searchedTag) {
+      onSave(createTag());
+    } else {
+      onSave(searchedTag);
+    }
+    setValue('');
+  };
+
+  const createTag = (): TagType => {
+    // TODO: create tag API를 적용해야됩니다.
+    const datetime = new Date().getTime();
+    return { id: datetime, content: '생성했다 치자' };
+  };
+
+  return (
+    <div css={formTagCss}>
+      <form onSubmit={onFormReturn}>
+        <SearchBar value={value} onChange={onChange} />
+      </form>
+      <AppleyedTags applyedTags={applyedTags} onRemove={onRemove} />
+      <RegisteredTagList registeredTags={registeredTags} onClick={onSave} />
+    </div>
+  );
+}
+
+const formTagCss = css`
+  display: flex;
+  flex-direction: column;
+  row-gap: 16px;
+`;
