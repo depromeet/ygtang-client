a86d2b471eb2bb698e807cd5065016b4a3120baa
positiveko <edie.spark1er@gmail.com>
2022-05-06 00:17:43 +0900

    feat: link page form 추가 및 버튼 disabled 기능 추가

--- src/components/add/LinkInput.tsx
+++ src/components/add/LinkInput.tsx
@@ -1,22 +1,14 @@
-import { useState } from 'react';
 import { css, Theme } from '@emotion/react';
 
 import { PlusIcon } from '~/components/common/icons';
+import { Input } from '~/components/common/TextField/Input';
+import LinkThumbnail from '~/components/LinkThumbnail';
 import useInput from '~/hooks/common/useInput';
+import { OpenGraph } from '~/pages/add/link';
 import { useToast } from '~/store/Toast';
 import { validator } from '~/utils/validator';
 
-import { Input } from '../common/TextField/Input';
-import LinkThumbnail from '../LinkThumbnail';
-
-interface OpenGraph {
-  description: string;
-  site_name: string;
-  title: string;
-  url: string;
-  imageUrl?: string;
-}
-
+// TODO openGraph 관련 api 개발 후 삭제 예정
 const TEMP_OG: OpenGraph = {
   description: '네이버 메인에서 다양한 정보와 유용한 컨텐츠를 만나 보세요',
   title: '네이버',
@@ -25,22 +17,23 @@ const TEMP_OG: OpenGraph = {
   imageUrl: 'https://s.pstatic.net/static/www/mobile/edit/2016/0705/mobile_212852414260.png',
 };
 
-function PlusBtn({ onClick }: { onClick: VoidFunction }) {
-  return (
-    <button css={btnCss} onClick={onClick}>
-      <PlusIcon color="white" />
-    </button>
-  );
+interface LinkInputProps {
+  openGraph: OpenGraph | null;
+  setOpenGraph: React.Dispatch<React.SetStateAction<OpenGraph | null>>;
 }
 
-export default function LinkInput() {
+export default function LinkInput({ openGraph, setOpenGraph }: LinkInputProps) {
+  const url = useInput({ useDebounce: true });
+
   const { fireToast } = useToast();
-  const [openGraph, setOpenGraph] = useState<OpenGraph | null>(null);
-  const url = useInput({});
 
   const showErrorMessage = () => fireToast({ content: '유효하지 않은 주소입니다.' });
 
-  const getOpenGraph = () => {
+  const getOpenGraph = (
+    e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    // TODO openGraph 관련 api 개발 후 수정 필요
     validator({ value: url.value, type: 'url' }) ? setOpenGraph(TEMP_OG) : showErrorMessage();
   };
 
@@ -49,7 +42,7 @@ export default function LinkInput() {
       {openGraph ? (
         <LinkThumbnail thumbnail={openGraph} edit onDelete={() => setOpenGraph(null)} />
       ) : (
-        <>
+        <form>
           <Input
             placeholder="영감이 되는 링크를 붙여 넣어보세요"
             fixedHeight={32}
@@ -57,9 +50,23 @@ export default function LinkInput() {
             onChange={url.onChange}
           />
           <PlusBtn onClick={getOpenGraph} />
-        </>
+        </form>
       )}
     </div>
+  );
+}
+
+function PlusBtn({
+  onClick,
+}: {
+  onClick: (
+    e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLElement, MouseEvent>
+  ) => void;
+}) {
+  return (
+    <button css={btnCss} onClick={onClick} type="submit">
+      <PlusIcon color="white" />
+    </button>
   );
 }
 
--- src/pages/add/link.tsx
+++ src/pages/add/link.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { css } from '@emotion/react';
 
 import LinkInput from '~/components/add/LinkInput';
@@ -5,36 +6,49 @@ import { CTAButton } from '~/components/common/Button';
 import NavigationBar from '~/components/common/NavigationBar';
 import { MemoText } from '~/components/common/TextField';
 
+export interface OpenGraph {
+  description: string;
+  site_name: string;
+  title: string;
+  url: string;
+  imageUrl?: string;
+}
+
 export default function AddLink() {
+  const [openGraph, setOpenGraph] = useState<OpenGraph | null>(null);
+
   return (
-    <article css={addTextCss}>
+    <article css={addLinkCss}>
       <NavigationBar title="링크 추가" />
-      <section css={addTextTopCss}>
+      <section css={addLinkTopCss}>
         <div css={linkBoxCss}>
-          <LinkInput />
+          <LinkInput openGraph={openGraph} setOpenGraph={setOpenGraph} />
         </div>
         <MemoText writable />
       </section>
-      <section css={addTextBottomCss}>
-        <CTAButton type="submit">Tang!</CTAButton>
+
+      <section css={addLinkBottomCss}>
+        <CTAButton disabled={!Boolean(openGraph)} type="submit">
+          Tang!
+        </CTAButton>
       </section>
     </article>
   );
 }
 
-const addTextCss = css`
+const addLinkCss = css`
   display: flex;
   flex-direction: column;
   height: 100vh;
   overflow: hidden;
 `;
 
-const addTextTopCss = css`
+const addLinkTopCss = css`
   flex-grow: 1;
   overflow-y: auto;
 `;
 
-const addTextBottomCss = css`
+const addLinkBottomCss = css`
   margin: 8px 0 16px 0;
 `;
 
