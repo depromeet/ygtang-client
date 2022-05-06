05d61c110d538897ff2606741485bdb0da886111
Hyesung Oh <haesungoh414@gmail.com>
2022-05-06 13:41:18 +0900

    test: useDidUpdate 테스트 코드 작성

---
+++ src/hooks/common/useDidUpdate/useDidUpdate.test.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import { cleanup as hookCleanup, renderHook } from '@testing-library/react-hooks';
+
+import useDidUpdate from './useDidUpdate';
+
+const STATE_BUTTON = 'setState';
+
+describe('hooks/common/useDidUpdate', () => {
+  let App: () => JSX.Element;
+  const mockCallback = jest.fn();
+
+  beforeEach(() => {
+    App = function () {
+      const [state, setState] = useState(false);
+
+      useDidUpdate(() => {
+        mockCallback();
+      }, [state]);
+
+      return (
+        <div>
+          <button onClick={() => setState(prev => !prev)}>{STATE_BUTTON}</button>
+        </div>
+      );
+    };
+  });
+
+  afterAll(() => {
+    cleanup();
+    hookCleanup();
+  });
+
+  it('should defined', () => {
+    expect(useDidUpdate).toBeDefined();
+  });
+
+  it('not called callback when mounted', () => {
+    const mockCallback = jest.fn();
+    renderHook(() => useDidUpdate(mockCallback, []));
+    expect(mockCallback).not.toBeCalled();
+  });
+
+  it('called when dependency list update', () => {
+    render(<App />);
+    expect(mockCallback).not.toBeCalled();
+    const setStateButton = screen.getByText(STATE_BUTTON);
+    fireEvent.click(setStateButton);
+    expect(mockCallback).toHaveBeenCalledTimes(1);
+  });
+});
