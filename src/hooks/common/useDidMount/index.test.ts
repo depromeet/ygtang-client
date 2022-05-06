aa4a66a6270eed1a8bb633a99333f1c98bad99c0
Hyesung Oh <haesungoh414@gmail.com>
2022-05-06 13:40:05 +0900

    test: useDidMount 테스트 코드 작성

---
+++ src/hooks/common/useDidMount/index.test.ts
@@ -0,0 +1,7 @@
+import Default from './index';
+
+describe('hooks/common/useDidMount', () => {
+  it('should defined with default', () => {
+    expect(Default).toBeDefined();
+  });
+});
---
+++ src/hooks/common/useDidMount/useDidMount.test.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import { cleanup as hookCleanup, renderHook } from '@testing-library/react-hooks';
+
+import useDidMount from './useDidMount';
+
+const STATE_BUTTON = 'setState';
+
+describe('hooks/common/useDidMount', () => {
+  let App: () => JSX.Element;
+  const mockCallback = jest.fn();
+
+  beforeEach(() => {
+    App = function () {
+      const [_, setState] = useState(false);
+
+      useDidMount(mockCallback);
+      return (
+        <div>
+          <button onClick={() => setState(prev => !prev)}>{STATE_BUTTON}</button>
+        </div>
+      );
+    };
+  });
+
+  afterAll(() => {
+    cleanup();
+    hookCleanup();
+  });
+
+  it('should defined with default', () => {
+    expect(useDidMount).toBeDefined();
+  });
+
+  it('called effect callback', () => {
+    const effectCallback = jest.fn();
+    renderHook(() => useDidMount(effectCallback));
+    expect(effectCallback).toBeCalled();
+  });
+
+  it('called once when rerender', () => {
+    const effectCallback = jest.fn();
+    const { rerender } = renderHook(() => useDidMount(effectCallback));
+    rerender();
+    expect(effectCallback).toHaveBeenCalledTimes(1);
+  });
+
+  it('called once when state changed', () => {
+    render(<App />);
+    expect(mockCallback).toHaveBeenCalledTimes(1);
+    const setStateButton = screen.getByText(STATE_BUTTON);
+    fireEvent.click(setStateButton);
+    expect(mockCallback).toHaveBeenCalledTimes(1);
+  });
+});
