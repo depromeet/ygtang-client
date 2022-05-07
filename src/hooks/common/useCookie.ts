3afeb9cf212525f0a06afd520958be3d10da30d9
doda <ddark.kr@gmail.com>
2022-05-08 05:12:20 +0900

    fix: useCookie import 문제 해결

--- src/hooks/common/useCookie.ts
+++ src/hooks/common/useCookie.ts
@@ -1,18 +1,18 @@
-import { get, remove, set } from 'js-cookie';
+import Cookie from 'js-cookie';
 
 import { IS_PRODUCTION } from '~/constants/common';
 
 export default function useCookie() {
   const handleSet = (key: string, value: any) => {
-    set(key, value, {
+    Cookie.set(key, value, {
       sameSite: 'Lax',
       secure: IS_PRODUCTION,
     });
   };
 
   return {
-    get,
+    get: Cookie.get,
     set: handleSet,
-    remove,
+    remove: Cookie.remove,
   };
 }
