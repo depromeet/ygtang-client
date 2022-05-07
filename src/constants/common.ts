1098454e5e8bd89de6ee2f8ac37b2a082fc03ce1
doda <ddark.kr@gmail.com>
2022-05-08 05:13:11 +0900

    chore: is_production 상수 수정

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,3 +1,3 @@
 export const PUBLIC_ROUTES = ['/login', '/password', '/signup'];
 
-export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+export const IS_PRODUCTION = process.env.APP_ENV === 'production';
