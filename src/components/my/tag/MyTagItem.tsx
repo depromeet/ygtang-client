bb5fbd46ee33d21373b5178e996823b6a71e80d9
정대윤 <sensecodevalue@gmail.com>
2022-05-08 03:53:17 +0900

    feat: MyTagItem 컴포넌트 추가

---
+++ src/components/my/tag/MyTagItem.tsx
@@ -0,0 +1,25 @@
+import { IconButton } from '~/components/common/Button';
+import Menu from '~/components/my/Menu';
+
+export default function MyTagItem({
+  tag,
+  onDelete,
+}: {
+  tag: TagType;
+  onDelete: (id: number) => void;
+}) {
+  return (
+    <Menu
+      label={tag.content}
+      rightElement={
+        <IconButton
+          onClick={() => {
+            onDelete(tag.id);
+          }}
+          iconName="RemoveCircleIcon"
+          light
+        />
+      }
+    />
+  );
+}
