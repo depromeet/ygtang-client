596ebfbb05a2b703d0d5fe844d306c25abe464cc
doda <ddark.kr@gmail.com>
2022-05-08 05:13:31 +0900

    feat: useReissueMutation을 만듬

---
+++ src/hooks/api/reissue/useReissueMutation.ts
@@ -0,0 +1,23 @@
+import { post } from '~/libs/api/client';
+import { poster } from '~/libs/api/fetcher';
+import makeApiMutation from '~/libs/api/makeApiMutation';
+import { AuthTokenResponseInterface } from '~/remotes/auth';
+
+interface ReissueMutationRequest {
+  accessToken: string;
+  refreshToken: string;
+}
+
+// NOTE: /v1/reissue endpoint는 커스텀 헤더를 요구합니다.
+async function customFetcher(input: ReissueMutationRequest) {
+  return post('/v1/reissue', input);
+}
+
+export function useReissueMutation() {
+  return makeApiMutation<ReissueMutationRequest, AuthTokenResponseInterface>(
+    'reissue',
+    '/v1/reissue',
+    poster,
+    customFetcher
+  );
+}
