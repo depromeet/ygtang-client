2bbd986c6d5ae182664c2b27027e9c4d934422ed
정대윤 <sensecodevalue@gmail.com>
2022-05-10 00:18:55 +0900

    fix: MyTagItem에 onDelete 위임 review적용

--- src/components/my/tag/MyTagItem.tsx
+++ src/components/my/tag/MyTagItem.tsx
@@ -1,25 +1,16 @@
 import { IconButton } from '~/components/common/Button';
 import Menu from '~/components/my/Menu';
 
-export default function MyTagItem({
-  tag,
-  onDelete,
-}: {
-  tag: TagType;
-  onDelete: (id: number) => void;
-}) {
+export default function MyTagItem({ tag }: { tag: TagType }) {
+  const onDelete = () => {
+    // TODO: Tag delete API 호출
+    console.log(tag.id);
+  };
+
   return (
     <Menu
       label={tag.content}
-      rightElement={
-        <IconButton
-          onClick={() => {
-            onDelete(tag.id);
-          }}
-          iconName="RemoveCircleIcon"
-          light
-        />
-      }
+      rightElement={<IconButton onClick={onDelete} iconName="RemoveCircleIcon" light />}
     />
   );
 }
