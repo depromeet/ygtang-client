9ea9db39e665878eba5a74c13da683d8ae290114
doda <ddark.kr@gmail.com>
2022-05-09 13:14:38 +0900

    feat: useMemberLoginMutation을 만듬

---
+++ src/hooks/api/member/useMemberLoginMutation.ts
@@ -0,0 +1,15 @@
+import { useMutation } from 'react-query';
+
+import { post } from '~/libs/api/client';
+
+export interface MemberLoginMutationRequest {
+  email: string;
+  password: string;
+}
+
+export default function useMemberLoginMutation() {
+  return useMutation<AuthTokenResponseInterface, { message?: string }, MemberLoginMutationRequest>(
+    (data: MemberLoginMutationRequest) =>
+      post<AuthTokenResponseInterface>('/v1/members/login', data)
+  );
+}
