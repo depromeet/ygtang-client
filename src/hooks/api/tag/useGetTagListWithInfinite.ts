451452f3fcc72b37272a719514cc809fad40d541
Hyesung Oh <haesungoh414@gmail.com>
2022-05-12 03:19:01 +0900

    feat: useGetTagListWithInfinite 검색 기능 추가

--- src/hooks/api/tag/useGetTagListWithInfinite.ts
+++ src/hooks/api/tag/useGetTagListWithInfinite.ts
@@ -14,17 +14,44 @@ interface TagDataResponseInterface extends PaginationInterface {
   content: TagInterface[];
 }
 
-export default function useGetTagListWithInfinite() {
-  const fetchTags = (page: number = 0) =>
-    get<TagResponseInterface>(`/v1/tag/list?size=2&page=${page}`);
+interface Props {
+  /**
+   * 검색을 위한 props 입니다.
+   * 선언하지 않거나, 길이가 0일 때 전체 List 조회됩니다.
+   */
+  keyword?: string;
+  /**
+   * 정확히 일치하는 검색 API 사용 여부입니다.
+   * @default false
+   * false일 시 유사한 검색 API가 사용됩니다.
+   */
+  isExactlySame?: boolean;
+}
+
+export default function useGetTagListWithInfinite({ keyword, isExactlySame = false }: Props) {
+  const fetchTags = (page: number = 0) => {
+    if (keyword && keyword.length > 0) {
+      // 일치하는 검색 API
+      if (isExactlySame)
+        return get<TagResponseInterface>(`/v1/tag/search/${keyword}?size=20&page=${page}`);
+      // 유사한 검색 API
+      return get<TagResponseInterface>(`/v1/tag/index/${keyword}?size=20&page=${page}`);
+    }
+    // 전체 Get API
+    return get<TagResponseInterface>(`/v1/tag/list?size=20&page=${page}`);
+  };
 
   const query = useInfiniteQuery<TagResponseInterface>(
-    [TAG_LIST_QUERY_KEY],
+    [TAG_LIST_QUERY_KEY, keyword, isExactlySame],
     async ({ pageParam = 0 }) => await fetchTags(pageParam),
     {
       getNextPageParam: lastPage => {
         if (lastPage.data.last) return undefined;
         return lastPage.data.number + 1 ?? undefined;
+      },
+      getPreviousPageParam: firstPage => {
+        if (firstPage.data.first) return undefined;
+        return firstPage.data.number - 1 ?? undefined;
       },
     }
   );
