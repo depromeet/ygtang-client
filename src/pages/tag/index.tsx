c58c411e8bd62d3c5763f14c2bdb8c1f8a879a40
Hyesung Oh <haesungoh414@gmail.com>
2022-05-13 00:45:54 +0900

    feat: Tag page에 LoadingHandler, FixedSpinner 적용

--- src/pages/tag/index.tsx
+++ src/pages/tag/index.tsx
@@ -1,21 +1,36 @@
+import { motion } from 'framer-motion';
+
+import LoadingHandler from '~/components/common/LoadingHandler';
 import NavigationBar from '~/components/common/NavigationBar';
+import { FixedSpinner } from '~/components/common/Spinner';
 import TagForm from '~/components/TagForm';
+import { defaultFadeInVariants } from '~/constants/motions';
 import useGetTagListWithInfinite from '~/hooks/api/tag/useGetTagListWithInfinite';
 import { useFilteredTags } from '~/store/FilteredTags';
 
 export default function TagPage() {
   const { filteredTags, addTag, removeTag } = useFilteredTags({});
-  const { tags } = useGetTagListWithInfinite({});
+  const { tags, isLoading } = useGetTagListWithInfinite({});
 
   return (
     <article>
       <NavigationBar title="태그 추가" backLink="/" backLinkScrollOption={false} />
-      <TagForm
-        applyedTags={filteredTags}
-        registeredTags={tags}
-        onSave={addTag}
-        onRemove={removeTag}
-      />
+
+      <LoadingHandler isLoading={isLoading} loadingComponent={<FixedSpinner />}>
+        <motion.div
+          variants={defaultFadeInVariants}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+        >
+          <TagForm
+            applyedTags={filteredTags}
+            registeredTags={tags}
+            onSave={addTag}
+            onRemove={removeTag}
+          />
+        </motion.div>
+      </LoadingHandler>
     </article>
   );
 }
