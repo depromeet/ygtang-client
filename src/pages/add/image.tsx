0ea55fd44a6d321b43233f429d0be982d5cec2bf
positiveko <edie.spark1er@gmail.com>
2022-05-13 02:16:55 +0900

    feat: image upload api 연결 및 useInspirationMutate 훅 생성

--- src/pages/add/image.tsx
+++ src/pages/add/image.tsx
@@ -5,30 +5,48 @@ import TagContent from '~/components/common/Content/TagContent';
 import ImageContent from '~/components/common/ImageContent';
 import NavigationBar from '~/components/common/NavigationBar';
 import { MemoText } from '~/components/common/TextField';
+import useInspirationMutation, {
+  InspirationMutationRequest,
+} from '~/hooks/api/inspiration/useInspirationMutation';
 import { useUploadedImg } from '~/store/UploadedImage';
 
 export default function AddImage() {
   const { uploadedImg } = useUploadedImg();
+  const { createInspiration } = useInspirationMutation();
 
   const tags = [{ id: 1, content: '1111' }];
+
+  const submitImg = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!uploadedImg) return;
+    const imgData: InspirationMutationRequest = {
+      file: uploadedImg,
+      memo: 'memo~',
+      tagIds: [1],
+      type: 'IMAGE',
+    };
+    createInspiration(imgData);
+  };
 
   return (
     <article css={addImageCss}>
       <NavigationBar title="이미지 추가" />
-      <section css={addImageTopCss}>
-        <div css={contentWrapperCss}>
-          {uploadedImg && <ImageContent src={uploadedImg} alt="uploadedImg" />}
-        </div>
-        <div css={contentWrapperCss}>
-          <TagContent onEdit={() => {}} tags={tags} />
-        </div>
-        <div css={contentWrapperCss}>
-          <MemoText writable />
-        </div>
-      </section>
-      <section css={addImageBottomCss}>
-        <CTAButton type="submit">Tang!</CTAButton>
-      </section>
+      <form onSubmit={submitImg}>
+        <section css={addImageTopCss}>
+          <div css={contentWrapperCss}>
+            {uploadedImg && <ImageContent src={uploadedImg} alt="uploadedImg" />}
+          </div>
+          <div css={contentWrapperCss}>
+            <TagContent onEdit={() => {}} tags={tags} />
+          </div>
+          <div css={contentWrapperCss}>
+            <MemoText writable />
+          </div>
+        </section>
+        <section css={addImageBottomCss}>
+          <CTAButton type="submit">Tang!</CTAButton>
+        </section>
+      </form>
     </article>
   );
 }
