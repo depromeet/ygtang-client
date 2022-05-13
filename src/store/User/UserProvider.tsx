8d6b8db759a9b416e84a02b10df8735cb325d623
Hyesung Oh <haesungoh414@gmail.com>
2022-05-13 15:27:42 +0900

    feat: UserProvider에서 useRouterGuard 사용 및 렌더링 조건 변경

--- src/store/User/UserProvider.tsx
+++ src/store/User/UserProvider.tsx
@@ -3,45 +3,40 @@ import { PropsWithChildren, useEffect } from 'react';
 import { COOKIE_REFRESH } from '~/constants/common';
 import useReissueMutation from '~/hooks/api/reissue/useReissueMutation';
 import useCookie from '~/hooks/common/useCookie';
+import useDidMount from '~/hooks/common/useDidMount';
+import useRouterGuard from '~/hooks/common/useRouterGuard';
 
 import { useUser } from './';
 
 export function UserProvider({ children }: PropsWithChildren<unknown>) {
-  const { isLoaded, setIsLoaded, userLogin } = useUser();
-  const {
-    mutate: reissueMutate,
-    data: reissueMutationData,
-    error: reissueMutationError,
-  } = useReissueMutation();
+  const { isLoaded, setIsLoaded, userLogin, isLoggedIn } = useUser();
+  const { mutate: reissueMutate, error: reissueMutationError } = useReissueMutation({
+    userLogin,
+    setIsLoaded,
+  });
   const { get: cookieGet } = useCookie();
 
+  const { isRouterGuardPassed } = useRouterGuard({ isLoaded, isLoggedIn });
+
   // 컴포넌트 마운트 시
-  useEffect(() => {
-    if (!isLoaded) {
-      const storedRefreshToken = cookieGet(COOKIE_REFRESH);
+  useDidMount(() => {
+    if (isLoaded) return;
 
-      if (storedRefreshToken) {
-        reissueMutate({
-          refreshToken: storedRefreshToken,
-        });
-      } else {
-        setIsLoaded(true);
-      }
-    }
-  }, [cookieGet, isLoaded, reissueMutate, setIsLoaded]);
+    const storedRefreshToken = cookieGet(COOKIE_REFRESH);
 
-  useEffect(() => {
-    if (reissueMutationData && reissueMutationData.data) {
-      const { accessToken, refreshToken } = reissueMutationData.data;
-      userLogin({ accessToken, refreshToken });
+    if (storedRefreshToken) {
+      reissueMutate({
+        refreshToken: storedRefreshToken,
+      });
+    } else {
       setIsLoaded(true);
     }
-  }, [reissueMutationData, setIsLoaded, userLogin]);
+  });
 
   useEffect(() => {
     // TODO: 에러 발생 핸들링
   }, [reissueMutationError]);
 
   // TODO: 로딩 관련 처리하기
-  return <>{isLoaded ? children : <>로그인 대기 중...</>}</>;
+  return <>{isRouterGuardPassed ? children : <>로그인 대기 중...</>}</>;
 }
