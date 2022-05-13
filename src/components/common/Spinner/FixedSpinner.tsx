e592482c4bb2bdacf4c7e3589155ffbd354b4772
Hyesung Oh <haesungoh414@gmail.com>
2022-05-13 16:40:34 +0900

    chore: FixedSpinner 100vh css var 대응

--- src/components/common/Spinner/FixedSpinner.tsx
+++ src/components/common/Spinner/FixedSpinner.tsx
@@ -15,7 +15,7 @@ const wrapperCss = css`
   left: 0;
   top: 0;
   width: 100vw;
-  height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
   display: flex;
   justify-content: center;
   align-items: center;
