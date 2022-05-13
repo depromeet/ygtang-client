aa8116acdc22e7d2dca7d70d4c1082bfb397904e
positiveko <edie.spark1er@gmail.com>
2022-05-14 06:19:37 +0900

    feat: inspiration 삭제 기능 추가

--- src/pages/content/[contentId].tsx
+++ src/pages/content/[contentId].tsx
@@ -6,16 +6,23 @@ import NavigationBar from '~/components/common/NavigationBar';
 import ImageView from '~/components/inspiration/ImageView';
 import LinkView from '~/components/inspiration/LinkView';
 import TextView from '~/components/inspiration/TextView';
+import useInspirationMutation from '~/hooks/api/inspiration/useInspirationMutation';
 import useInternalRouter from '~/hooks/common/useInternalRouter';
 import { useInspirationDetail } from '~/store/Inspiration';
 
 export default function ContentPage() {
   const { inspirationDetail } = useInspirationDetail();
+  const { deleteInspiration } = useInspirationMutation();
   const { push } = useInternalRouter();
 
   useEffect(() => {
     if (!inspirationDetail) return push('/');
   }, [inspirationDetail, push]);
+
+  const deleteInspirationById = (id: number) => {
+    // TODO: 삭제 여부 묻는 다이얼로그 추가 필요
+    deleteInspiration(id);
+  };
 
   const renderInspirationViewByType = useCallback(() => {
     const type = inspirationDetail?.type;
@@ -29,6 +36,7 @@ export default function ContentPage() {
   }, [inspirationDetail?.type]);
 
   if (!inspirationDetail) return <></>;
+  const { id } = inspirationDetail;
 
   return (
     <article css={contentCss}>
@@ -36,7 +44,9 @@ export default function ContentPage() {
         title=""
         backLink="/"
         backLinkScrollOption={false}
-        rightElement={<IconButton iconName="DeleteIcon" light />}
+        rightElement={
+          <IconButton onClick={() => deleteInspirationById(id)} iconName="DeleteIcon" light />
+        }
       />
       {renderInspirationViewByType()}
     </article>
