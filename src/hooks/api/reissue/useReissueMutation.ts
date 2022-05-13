07dcda38126b433fa7a32fdbb04aa09d9788fe9a
Hyesung Oh <haesungoh414@gmail.com>
2022-05-13 15:26:42 +0900

    fix: useReissueMutation onSuccess 콜백 적용

--- src/hooks/api/reissue/useReissueMutation.ts
+++ src/hooks/api/reissue/useReissueMutation.ts
@@ -1,3 +1,4 @@
+import { Dispatch, SetStateAction } from 'react';
 import { useMutation } from 'react-query';
 
 import { post } from '~/libs/api/client';
@@ -6,12 +7,25 @@ interface ReissueMutationRequest {
   refreshToken: string;
 }
 
-export default function useReissueMutation() {
-  return useMutation((data: ReissueMutationRequest) =>
-    post<AuthTokenResponseInterface>('/v1/reissue', undefined, {
-      headers: {
-        'REFRESH-TOKEN': data.refreshToken,
+interface UseReissueMutationProps {
+  userLogin: ({ accessToken, refreshToken }: { accessToken: string; refreshToken: string }) => void;
+  setIsLoaded: Dispatch<SetStateAction<boolean>>;
+}
+
+export default function useReissueMutation({ userLogin, setIsLoaded }: UseReissueMutationProps) {
+  return useMutation(
+    (data: ReissueMutationRequest) =>
+      post<AuthTokenResponseInterface>('/v1/reissue', undefined, {
+        headers: {
+          'REFRESH-TOKEN': data.refreshToken,
+        },
+      }),
+    {
+      onSuccess: data => {
+        const { accessToken, refreshToken } = data.data;
+        userLogin({ accessToken, refreshToken });
+        setIsLoaded(true);
       },
-    })
+    }
   );
 }
