060d56898f58520f309f5b1be928132992a7d30f
정대윤 <sensecodevalue@gmail.com>
2022-05-13 16:56:35 +0900

    feat: createTagMutation type 추가

--- src/hooks/api/tag/useTagMutation.ts
+++ src/hooks/api/tag/useTagMutation.ts
@@ -4,14 +4,27 @@ import { del, post } from '~/libs/api/client';
 
 import useTagRefresh from './useTagRefresh';
 
+interface CreateTagResponseInterface {
+  message: string;
+  data: CreateTagDataResponseInterface;
+}
+
+interface CreateTagDataResponseInterface {
+  id: number;
+  content: string;
+}
+
 export default function useTagMutation() {
   const { refresh } = useTagRefresh();
 
-  const createTagMutation = useMutation((content: string) => post('/v1/tag/add', { content }), {
-    onSuccess: () => {
-      refresh();
-    },
-  });
+  const createTagMutation = useMutation<CreateTagResponseInterface, { message?: string }, string>(
+    (content: string) => post<CreateTagResponseInterface>('/v1/tag/add', { content }),
+    {
+      onSuccess: () => {
+        refresh();
+      },
+    }
+  );
 
   const deleteTagMutation = useMutation((id: number) => del(`/v1/tag/remove/${id}`), {
     onSuccess: () => {
