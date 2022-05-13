0f96bd98e21bb23b9d5fed32beb7b76ae692cb62
doda <ddark.kr@gmail.com>
2022-05-14 05:06:52 +0900

    feat: 마이페이지에 일러스트 다이얼로그 띄움

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -1,7 +1,30 @@
 import { css } from '@emotion/react';
 
+import { FilledButton } from '~/components/common/Button';
+import IllustDialog from '~/components/common/IllustDialog';
+import { INSPIRATION_MODAL_IMAGE } from '~/constants/assets';
+import useInternalRouter from '~/hooks/common/useInternalRouter';
+
 export default function MyPage() {
-  return <article css={myPageCss}>마이 페이지</article>;
+  const { push } = useInternalRouter();
+  return (
+    <article css={myPageCss}>
+      <IllustDialog
+        isShowing={true}
+        image={INSPIRATION_MODAL_IMAGE[2]}
+        actionButtons={
+          <>
+            <FilledButton colorType="light" onClick={() => push('/')}>
+              네! 이해할 수 있어요.
+            </FilledButton>
+          </>
+        }
+      >
+        아직 개발 중이에요.
+        <br /> 빠르게 사용할 수 있도록 노력할게요!
+      </IllustDialog>
+    </article>
+  );
 }
 
 const myPageCss = css``;
