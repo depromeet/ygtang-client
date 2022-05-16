6c5dba55113b99d5d36b9ff1b4c65001c4dac0fe
Hyesung Oh <haesungoh414@gmail.com>
2022-05-16 13:44:59 +0900

    feat: 홈 태그 필터 화면 완료 버튼 추가

--- src/pages/tag/index.tsx
+++ src/pages/tag/index.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import { motion } from 'framer-motion';
 
+import { GhostButton } from '~/components/common/Button';
 import LoadingHandler from '~/components/common/LoadingHandler';
 import NavigationBar from '~/components/common/NavigationBar';
 import { FixedSpinner } from '~/components/common/Spinner';
 import TagForm from '~/components/TagForm';
 import { defaultFadeInVariants } from '~/constants/motions';
 import useGetTagListWithInfinite from '~/hooks/api/tag/useGetTagListWithInfinite';
+import useInternalRouter from '~/hooks/common/useInternalRouter';
 import { useFilteredTags } from '~/store/FilteredTags';
 
 export default function TagPage() {
@@ -14,9 +16,24 @@ export default function TagPage() {
   const [keyword, setKeyword] = useState('');
   const { tags, isLoading } = useGetTagListWithInfinite({ keyword });
 
+  const router = useInternalRouter();
+
+  const onClickComplete = () => {
+    router.scrollPreventedPush('/');
+  };
+
   return (
     <article>
-      <NavigationBar title="태그 필터" backLink="/" backLinkScrollOption={false} />
+      <NavigationBar
+        title="태그 필터"
+        backLink="/"
+        backLinkScrollOption={false}
+        rightElement={
+          <GhostButton size="large" onClick={onClickComplete}>
+            완료
+          </GhostButton>
+        }
+      />
 
       <LoadingHandler isLoading={isLoading} loadingComponent={<FixedSpinner />}>
         <motion.div
