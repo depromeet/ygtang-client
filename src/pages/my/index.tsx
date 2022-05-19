031186c763c746ebe87e8d895e11630cbd2b0606
정대윤 <sensecodevalue@gmail.com>
2022-05-19 21:41:39 +0900

    feat: MyPage UI 반영

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -1,30 +1,61 @@
 import { css } from '@emotion/react';
 
-import { FilledButton } from '~/components/common/Button';
-import IllustDialog from '~/components/common/IllustDialog';
-import { INSPIRATION_MODAL_IMAGE } from '~/constants/assets';
-import useInternalRouter from '~/hooks/common/useInternalRouter';
+import NavigationBar from '~/components/common/NavigationBar';
+import Menu from '~/components/my/Menu';
+import MyProfile from '~/components/my/Profile';
 
 export default function MyPage() {
-  const { push } = useInternalRouter();
   return (
-    <article css={myPageCss}>
-      <IllustDialog
-        isShowing={true}
-        image={INSPIRATION_MODAL_IMAGE[2]}
-        actionButtons={
-          <>
-            <FilledButton colorType="light" onClick={() => push('/')}>
-              네! 이해할 수 있어요.
-            </FilledButton>
-          </>
-        }
-      >
-        아직 개발 중이에요.
-        <br /> 빠르게 사용할 수 있도록 노력할게요!
-      </IllustDialog>
+    <article css={myPageContainerCss}>
+      <NavigationBar title="환경설정" />
+      <section css={myPageCss}>
+        <MyProfile />
+        <ul>
+          <Menu
+            label="내계정"
+            onClick={() => {
+              console.log('내계정');
+            }}
+          />
+          <Menu
+            label="태그관리"
+            href="/my/tag"
+            onClick={() => {
+              console.log('태그관리');
+            }}
+          />
+          <Menu label="이용약관" />
+          <Menu
+            label="개인정보 정책"
+            onClick={() => {
+              console.log('이용약관');
+            }}
+          />
+          <Menu
+            css={initalizeMenuCss}
+            label="정보초기화"
+            onClick={() => {
+              console.log('정보초기화');
+            }}
+          />
+        </ul>
+      </section>
     </article>
   );
 }
 
-const myPageCss = css``;
+const myPageContainerCss = css`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const myPageCss = css`
+  flex: 1;
+  overflow-y: auto;
+`;
+
+const initalizeMenuCss = css`
+  margin-top: 64px;
+`;
