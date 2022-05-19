40e142ac43e289ce2a7d022cd395cacf45b6ae22
정대윤 <sensecodevalue@gmail.com>
2022-05-19 21:41:12 +0900

    feat: Menu 컴포넌트에 link 기능 추가

--- src/components/my/Menu.tsx
+++ src/components/my/Menu.tsx
@@ -1,17 +1,41 @@
-import { ReactElement } from 'react';
+import { ReactElement, useRef } from 'react';
 import { css, Theme } from '@emotion/react';
+
+import { RouterPathType } from '~/hooks/common/useInternalRouter';
+
+import InternalLink from '../common/InternalLink';
 
 export interface MenuProps {
   label: string;
   rightElement?: ReactElement;
+  href?: RouterPathType;
+  onClick?: VoidFunction;
 }
 
-export default function Menu({ label, rightElement }: MenuProps) {
+export default function Menu({ label, rightElement, href, onClick, ...props }: MenuProps) {
+  const $link = useRef<HTMLDivElement>(null);
+
+  const onClickHandler = (e: React.MouseEvent<HTMLSpanElement>) => {
+    if (e.target !== e.currentTarget) return;
+    if (href) {
+      $link.current?.click();
+    } else {
+      onClick && onClick();
+    }
+  };
+
   return (
-    <li css={MenuCss}>
-      <span css={menuTitleCss}>{label}</span>
-      {rightElement && <>{rightElement}</>}
-    </li>
+    <>
+      {href && (
+        <InternalLink css={hiddenCss} href={href}>
+          <span ref={$link}></span>
+        </InternalLink>
+      )}
+      <li css={MenuCss} {...props} onClick={onClickHandler}>
+        <span css={menuTitleCss}>{label}</span>
+        {rightElement && <>{rightElement}</>}
+      </li>
+    </>
   );
 }
 
@@ -27,4 +51,15 @@ const MenuCss = (theme: Theme) => css`
 
 const menuTitleCss = css`
   font-size: 12px;
+`;
+
+const hiddenCss = css`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  padding: 0;
+  overflow: hidden;
+  border: 0;
+  clip: rect(0 0 0 0);
 `;
