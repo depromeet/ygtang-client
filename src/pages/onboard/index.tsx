a835851d9a5da6981a484cdcf1a93286f56f0885
Hyesung Oh <haesungoh414@gmail.com>
2022-05-20 13:01:49 +0900

    design: /onboard route CTA 버튼 하단 위치

--- src/pages/onboard/index.tsx
+++ src/pages/onboard/index.tsx
@@ -6,39 +6,42 @@ import { Indicator, SnapCarousel } from '~/components/common/SnapCarousel';
 import { OnboardContent } from '~/components/onboard';
 import { ONBOARD_IMAGE } from '~/constants/assets';
 import useInternalRouter from '~/hooks/common/useInternalRouter';
+import { fullViewHeight } from '~/styles/utils';
 
 export default function Onboard() {
   const { onClickCTA, onClickSignUp } = useOnboardRouter();
   const [carouselWrapperRef, setCarouselWrapperRef] = useState<HTMLDivElement | null>(null);
 
   return (
-    <article>
-      <SnapCarousel.Wrapper ref={setCarouselWrapperRef}>
-        <SnapCarousel.Item>
-          <OnboardContent
-            imgSrc={ONBOARD_IMAGE[0]}
-            title="거기, 영감 있나요?"
-            description="혹시, 여기저기서 수집한 영감들이<br/>어딘가에 흩어져 있지 않나요?"
-          />
-        </SnapCarousel.Item>
-        <SnapCarousel.Item>
-          <OnboardContent
-            imgSrc={ONBOARD_IMAGE[1]}
-            title="마음껏 추가하세요."
-            description="글, 사진, 링크로 된 영감을 모을 수 있어요."
-          />
-        </SnapCarousel.Item>
-        <SnapCarousel.Item>
-          <OnboardContent
-            imgSrc={ONBOARD_IMAGE[2]}
-            title="나의 영감이 Tang!"
-            description="언제든 찾아볼 수 있도록 나만의 태그를 추가하고,<br/> 메모를 남겨보세요. 그리고, Tang!"
-          />
-        </SnapCarousel.Item>
-      </SnapCarousel.Wrapper>
+    <article css={wrapperCss}>
+      <div css={onboardContentWrapperCss}>
+        <SnapCarousel.Wrapper ref={setCarouselWrapperRef}>
+          <SnapCarousel.Item>
+            <OnboardContent
+              imgSrc={ONBOARD_IMAGE[0]}
+              title="거기, 영감 있나요?"
+              description="혹시, 여기저기서 수집한 영감들이<br/>어딘가에 흩어져 있지 않나요?"
+            />
+          </SnapCarousel.Item>
+          <SnapCarousel.Item>
+            <OnboardContent
+              imgSrc={ONBOARD_IMAGE[1]}
+              title="마음껏 추가하세요."
+              description="글, 사진, 링크로 된 영감을 모을 수 있어요."
+            />
+          </SnapCarousel.Item>
+          <SnapCarousel.Item>
+            <OnboardContent
+              imgSrc={ONBOARD_IMAGE[2]}
+              title="나의 영감이 Tang!"
+              description="언제든 찾아볼 수 있도록 나만의 태그를 추가하고,<br/> 메모를 남겨보세요. 그리고, Tang!"
+            />
+          </SnapCarousel.Item>
+        </SnapCarousel.Wrapper>
 
-      <div css={indicatorWrapperCss}>
-        <Indicator wrapperRef={carouselWrapperRef} />
+        <div css={indicatorWrapperCss}>
+          <Indicator wrapperRef={carouselWrapperRef} />
+        </div>
       </div>
 
       <CTAButton onClick={onClickCTA}>시작하기</CTAButton>
@@ -51,6 +54,20 @@ export default function Onboard() {
     </article>
   );
 }
+
+const wrapperCss = css`
+  display: flex;
+  flex-direction: column;
+  min-height: ${fullViewHeight()};
+`;
+
+const onboardContentWrapperCss = css`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const indicatorWrapperCss = css`
   margin-top: 45px;
