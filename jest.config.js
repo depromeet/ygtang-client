74c95ebaed22732f2a7d50c4b5a9c5e794808509
Hyesung Oh <haesungoh414@gmail.com>
2022-05-20 17:25:29 +0900

    env: 테스트 커버리지 디렉토리 수정

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,13 @@ const customJestConfig = {
   moduleDirectories: ['node_modules'],
   testEnvironment: 'jsdom',
   testRegex: '(/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$',
-  collectCoverageFrom: ['**/src/**/*.{js,ts,jsx,tsx}'],
+  collectCoverageFrom: [
+    '**/src/**/*.{js,ts,jsx,tsx}',
+    '!**/src/constants/**/*.{js,ts,jsx,tsx}',
+    '!**/src/remotes/**/*.d.ts',
+    '!**/src/store/**/!(use)*.{js,ts,jsx,tsx}',
+    '!**/src/styles/!(utils)/*.{js,ts,jsx,tsx}'
+  ],
   moduleNameMapper: {
     '~/(.*)': '<rootDir>/src/$1',
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|ico)$': 'identity-obj-proxy',
