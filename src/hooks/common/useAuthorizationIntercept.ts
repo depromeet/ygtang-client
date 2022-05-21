5d45f3ed6f928d946ddfbf4201a4331c8840936b
doda <ddark.kr@gmail.com>
2022-05-21 16:52:22 +0900

    refactor: 리뷰 반영 (fail-fast | early return)
    thanks to @SenseCodeValue

--- src/hooks/common/useAuthorizationIntercept.ts
+++ src/hooks/common/useAuthorizationIntercept.ts
@@ -20,39 +20,38 @@ export default function useAuthorizationIntercept() {
     const axiosRejectedInterceptor = async (error: AxiosError) => {
       const status = error.response ? error.response.status : null;
 
-      if (error.config.headers && status === 401 && !isPending) {
-        if (!refreshToken) {
-          // TODO: 추후 로그아웃(user/userLogout) 넣기
-          return Promise.reject('로그인 하지 못했습니다.');
-        }
-        setPending(true);
-        try {
-          const { data } = await post<{ message: string; data: AuthTokenResponseInterface }>(
-            '/v1/reissue',
-            undefined,
-            {
-              headers: {
-                'REFRESH-TOKEN': refreshToken,
-              },
-            }
-          );
-          setPending(false);
-          userLogin({ accessToken: data.accessToken, refreshToken: data.refreshToken });
-          error.config.headers['accessToken'] = data.accessToken;
-        } catch {
-          fireToast({ content: '세션이 만료되었습니다. 다시 로그인 해주세요.' });
+      if (!error.config.headers || status !== 401 || isPending)
+        return Promise.reject(new Error(error.response?.data?.message ?? error));
+      if (!refreshToken) {
+        // TODO: 추후 로그아웃(user/userLogout) 넣기
+        return Promise.reject('로그인 하지 못했습니다.');
+      }
+      setPending(true);
 
-          // TODO: 추후 로그아웃(user/userLogout)으로 대체
-          push('/login');
-          removeCookie(COOKIE_REFRESH);
-          setPending(false);
-          return Promise.reject('세션이 만료되었습니다.');
-        }
+      try {
+        const { data } = await post<{ message: string; data: AuthTokenResponseInterface }>(
+          '/v1/reissue',
+          undefined,
+          {
+            headers: {
+              'REFRESH-TOKEN': refreshToken,
+            },
+          }
+        );
+        setPending(false);
+        userLogin({ accessToken: data.accessToken, refreshToken: data.refreshToken });
+        error.config.headers['accessToken'] = data.accessToken;
+      } catch {
+        fireToast({ content: '세션이 만료되었습니다. 다시 로그인 해주세요.' });
 
-        return instance.request(error.config);
+        // TODO: 추후 로그아웃(user/userLogout)으로 대체
+        push('/onboard');
+        removeCookie(COOKIE_REFRESH);
+        setPending(false);
+        return Promise.reject('세션이 만료되었습니다.');
       }
 
-      return Promise.reject(new Error(error.response?.data?.message ?? error));
+      return instance.request(error.config);
     };
 
     replaceResponseInterceptorForApiInstance({ rejected: axiosRejectedInterceptor });
