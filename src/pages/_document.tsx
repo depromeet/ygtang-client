d42b6df73e7a43c2b3b458d1a40eed6fbf360568
Hyesung Oh <haesungoh414@gmail.com>
2022-05-21 21:37:44 +0900

    fix: _document 오타 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,7 +25,7 @@ export default function Document() {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.GA_TRACKING_ID}');
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
             `,
               }}
             ></script>
