73bddf10579b02c5742a14e43bc74dded9b0878e
정대윤 <sensecodevalue@gmail.com>
2022-05-22 17:59:15 +0900

    feat: 닉네임 변경 페이지 추가

---
+++ src/pages/my/account/change-nickname.tsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import { css } from '@emotion/react';
+
+import useGetUserInformation, {
+  UserInformationInterface,
+} from '~/hooks/api/member/useGetUserInfromation';
+import useDidUpdate from '~/hooks/common/useDidUpdate';
+import useInput from '~/hooks/common/useInput';
+
+import { GhostButton } from '../../../components/common/Button';
+import NavigationBar from '../../../components/common/NavigationBar';
+import TextField from '../../../components/common/TextField';
+
+export default function MyAccountChangeNickame() {
+  const nickname = useInput({ useDebounce: true });
+  const initNickname = (userInfromation: UserInformationInterface) => {
+    nickname.setValue(userInfromation.nickName);
+  };
+  const { userInfromation } = useGetUserInformation(initNickname);
+  const [nicknameError, setNicknameError] = useState('');
+
+  useDidUpdate(() => {
+    if (!nickname.debouncedValue.length || userInfromation?.nickName === nickname.debouncedValue) {
+      setNicknameError('변경될 이름을 입력해주세요.');
+    } else {
+      setNicknameError('');
+    }
+  }, [nickname.debouncedValue]);
+
+  const onFormReturn = async (e: React.FormEvent) => {
+    e.preventDefault();
+    callMuation();
+  };
+
+  const callMuation = () => {
+    console.log('뮤테이션 실행');
+  };
+
+  return (
+    <article css={editNickNameCss}>
+      <NavigationBar
+        title="이름"
+        rightElement={
+          <GhostButton
+            size="large"
+            onClick={() => {
+              callMuation();
+            }}
+          >
+            완료
+          </GhostButton>
+        }
+      />
+      <form css={formCss} onSubmit={onFormReturn}>
+        <TextField
+          label="이름"
+          feedback={nickname.debouncedValue !== '' ? nicknameError || <>&nbsp;</> : <>&nbsp;</>}
+          isSuccess={nickname.debouncedValue.length > 0 && nicknameError === ''}
+          value={nickname.value}
+          onChange={nickname.onChange}
+          required
+        />
+      </form>
+    </article>
+  );
+}
+
+const editNickNameCss = css`
+  display: flex;
+  flex-direction: column;
+`;
+const formCss = css`
+  margin-top: 32px;
+`;
