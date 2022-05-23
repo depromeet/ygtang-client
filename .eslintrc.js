061c6d9974d9aab5b4fe37b9b7e82a3214d8b961
hyesung oh <haesungoh414@gmail.com>
2022-05-24 00:28:26 +0900

    E2E 테스트 환경 및 CI 구성 w/ Cypress (#283)
    * test: cypress 설치
    
    * env: cypress eslint 설정
    
    * test: cypress 설치 및 gitignore 적용
    
    * env: cypress ci 설정
    
    * env: CI에 mkcert 적용
    
    * fix: CI typo 수정
    
    * fix: CI 브라우저 설정 해제
    
    * fix: CI wait-on 설정 해제
    
    * feat: CI 브라우저 설정
    
    * fix: CI 비필요 설치 커맨드 삭제
    
    * fix: Cypress CI Container 해제

--- .eslintrc.js
+++ .eslintrc.js
@@ -58,8 +58,19 @@ module.exports = {
       env: {
         jest: true,
       },
-      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(test).[jt]s?(x)'],
       extends: ['plugin:testing-library/react', 'plugin:jest/recommended'],
+      rules: {
+        // 테스트 환경에서 devDependency 사용을 위해
+        'import/no-extraneous-dependencies': [
+          'off',
+          { devDependencies: ['**/?(*.)+(spec|test).[jt]s?(x)'] },
+        ],
+        'react/display-name': 'off',
+      },
+    },
+    {
+      files: ['**/cypress/**/*.[jt]s?(x)', '**/?(*.)+(spec).[jt]s?(x)'],
       rules: {
         // 테스트 환경에서 devDependency 사용을 위해
         'import/no-extraneous-dependencies': [
