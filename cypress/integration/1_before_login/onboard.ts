bf44bd249ffb0fcb54131a5dfa115198081d1d04
Hyesung Oh <haesungoh414@gmail.com>
2022-05-23 23:30:37 +0900

    feat: CI 브라우저 설정

--- cypress/integration/1_before_login/onboard.ts
+++ cypress/integration/1_before_login/onboard.ts
@@ -1,4 +1,4 @@
-describe('home/navigation', () => {
+describe('1_before_login/onboard', () => {
   it('비로그인, root 접근 시 onboard로 이동', () => {
     cy.visit('/');
     cy.url().should('include', '/onboard');
