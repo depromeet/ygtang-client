fde1bf79b28324f0f4a3d473cbd903bb461c54f7
doda <ddark.kr@gmail.com>
2022-05-25 00:57:54 +0900

    feat: 비밀번호 변경 페이지 구성

--- src/pages/my/account/change-password.tsx
+++ src/pages/my/account/change-password.tsx
@@ -1,72 +1,146 @@
+import { FormEvent, useState } from 'react';
 import { css, Theme } from '@emotion/react';
 
-import { CTAButton } from '~/components/common/Button';
+import { CTABottomButton } from '~/components/common/Button';
 import NavigationBar from '~/components/common/NavigationBar';
-import { CONNECT_EMAIL } from '~/constants/common';
+import TextField from '~/components/common/TextField';
+import { useChangePassword } from '~/hooks/api/member/useChangePassword';
+import useDidUpdate from '~/hooks/common/useDidUpdate';
+import useInput from '~/hooks/common/useInput';
+import useInternalRouter from '~/hooks/common/useInternalRouter';
 import { useToast } from '~/store/Toast';
-import { useUserInformation } from '~/store/UserInformation';
+import { validator } from '~/utils/validator';
 
-export default function MyAccountChangePassword() {
+export default function MypageChangePassword() {
+  const { push } = useInternalRouter();
   const { fireToast } = useToast();
 
-  const { userInformation } = useUserInformation();
+  const { mutate: changePasswordMutate, isLoading: changePasswordLoading } = useChangePassword({
+    onSuccess: () => {
+      fireToast({
+        content: '비밀번호가 변경되었습니다.',
+      });
+      push('/my');
+    },
+    onError: () => {
+      fireToast({
+        content: '비밀번호 변경 시 오류가 발생하였습니다.',
+      });
+    },
+  });
+
+  const password = useInput({ useDebounce: true });
+  const passwordRepeat = useInput({ useDebounce: true });
+  const [passwordError, setPasswordError] = useState('');
+  const [passwordRepeatError, setPasswordRepeatError] = useState('');
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (changePasswordLoading) return;
+    if (passwordError === '' && passwordRepeatError === '') {
+      changePasswordMutate({
+        password: password.value,
+        confirmPassword: passwordRepeat.value,
+      });
+    }
+  };
+
+  useDidUpdate(() => {
+    if (password.debouncedValue.length >= 6) {
+      if (
+        validator({
+          rule: /^(?=.*[A-Za-z])(?=.*\d)[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/,
+          value: password.debouncedValue,
+        })
+      ) {
+        setPasswordError('');
+      } else {
+        setPasswordError('비밀번호는 영문과 숫자를 모두 포함하여야 합니다.');
+      }
+    } else {
+      setPasswordError('비밀번호는 6자리 이상이여야 합니다.');
+    }
+  }, [password.debouncedValue]);
+
+  useDidUpdate(() => {
+    if (
+      passwordRepeat.debouncedValue.length > 0 &&
+      passwordRepeat.debouncedValue === password.value
+    ) {
+      setPasswordRepeatError('');
+    } else {
+      setPasswordRepeatError('비밀번호와 일치하여야 합니다.');
+    }
+  }, [passwordRepeat.debouncedValue]);
 
   return (
-    <article css={loginCss}>
-      <NavigationBar title="비밀번호 변경?찾기?" />
-      <div css={introCardCss}>
-        <p css={introTextWrapper}>
-          안녕하세요. {userInformation.nickName}님
-          <br />
-          비밀번호를 변경하고 싶으신가요?
-          <br />
-          비밀번호를 다시 설정하시려면
-          <br />
-          아래버튼을 클릭해주세요.
-        </p>
-      </div>
-      <CTAButton
-        onClick={() => {
-          fireToast({
-            content:
-              '메일함을 확인해주세요. 등록된 이메일주소로 비밀번호를 재설정할 수 있는 링크를 보내드렸습니다.',
-          });
-        }}
-      >
-        비밀번호 변경하기
-      </CTAButton>
-      <div css={connetUsTextCss}>문의사항은 {CONNECT_EMAIL}로 문의해 주세요.</div>
+    <article css={containerCss}>
+      <NavigationBar title={'비밀번호 찾기'} />
+      <p css={introTextWrapper}>비밀번호를 변경합니다.</p>
+      <form css={formCss} onSubmit={handleSubmit}>
+        <fieldset css={fieldSetCss}>
+          <TextField
+            type="password"
+            label={'비밀번호'}
+            placeholder={'영문, 숫자 포함 6자 이상의 비밀번호'}
+            feedback={password.debouncedValue !== '' ? passwordError || <>&nbsp;</> : <>&nbsp;</>}
+            isSuccess={password.debouncedValue.length > 0 && passwordError === ''}
+            value={password.value}
+            onChange={password.onChange}
+            required
+          />
+          <TextField
+            type="password"
+            label={'비밀번호 확인'}
+            placeholder={'영문, 숫자 포함 6자 이상의 비밀번호'}
+            feedback={
+              passwordRepeat.debouncedValue !== '' ? (
+                passwordRepeatError || <>&nbsp;</>
+              ) : (
+                <>&nbsp;</>
+              )
+            }
+            isSuccess={passwordRepeat.debouncedValue.length > 0 && passwordRepeatError === ''}
+            value={passwordRepeat.value}
+            onChange={passwordRepeat.onChange}
+            required
+          />
+        </fieldset>
+        <CTABottomButton type={'submit'} disabled={changePasswordLoading}>
+          변경
+        </CTABottomButton>
+      </form>
     </article>
   );
 }
 
-const loginCss = css`
+const containerCss = css`
+  display: flex;
+  flex-direction: column;
+
+  height: 100%;
+`;
+
+const introTextWrapper = (theme: Theme) => css`
+  white-space: pre;
+  font-weight: ${theme.font.weight.bold};
+  color: ${theme.color.gray05};
+  font-size: 18px;
+  line-height: 150%;
+  margin-top: 40px;
+  margin-bottom: 32px;
+`;
+
+const formCss = css`
+  height: 100%;
   display: flex;
   flex-direction: column;
 `;
 
-const introCardCss = css`
-  position: relative;
-  width: 100%;
-  height: 108px;
-  margin-top: 40px;
-  margin-bottom: 64px;
-`;
+const fieldSetCss = css`
+  display: flex;
+  flex-direction: column;
+  gap: 16px; // (original 36px) - (label height 20px)
 
-const introTextWrapper = (theme: Theme) => css`
-  position: absolute;
-  left: 12px;
-  bottom: 17px;
-  color: ${theme.color.gray05};
-  font-size: 15px;
-  font-weight: 600;
-  line-height: 150%;
-`;
-
-const connetUsTextCss = (theme: Theme) => css`
-  margin-top: 44px;
-  font-size: 12px;
-  line-height: 150%;
-  text-align: center;
-  color: ${theme.color.gray03};
+  flex-grow: 1;
 `;
