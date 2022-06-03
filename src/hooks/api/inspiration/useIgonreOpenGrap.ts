7e7ae7779ed6f2d6b4844dc229db986f414b7fd6
정대윤 <sensecodevalue@gmail.com>
2022-06-04 01:56:55 +0900

    feat: og가 막힌 것을 체크 및 그것을 통한 og를 만들어 내는 useIgonreOpenGrap hooks 추가

---
+++ src/hooks/api/inspiration/useIgonreOpenGrap.ts
@@ -0,0 +1,33 @@
+import { OpenGraph } from '~/components/inspiration/LinkView';
+
+const INSTAGRAM_ORIGIN = 'www.instagram.com';
+const FACEBOOK_ORIGIN = 'www.facebook.com';
+const IGONRE_OPEN_GRAPH_HOSTS = [INSTAGRAM_ORIGIN, FACEBOOK_ORIGIN];
+
+const SUCCESS_CODE = 200;
+
+export default function useIgonreOpenGrap() {
+  const checkIgonreOpenGraphHost = (_url: string) => {
+    try {
+      const { host } = new URL(_url);
+      return IGONRE_OPEN_GRAPH_HOSTS.includes(host);
+    } catch {
+      return false;
+    }
+  };
+  const makeURLOpenGraph = (_url: string): OpenGraph | undefined => {
+    const { origin, hostname } = new URL(_url);
+    return {
+      description: '',
+      siteName: hostname,
+      title: origin,
+      url: _url,
+      code: SUCCESS_CODE,
+    };
+  };
+
+  return {
+    checkIgonreOpenGraphHost,
+    makeURLOpenGraph,
+  };
+}
