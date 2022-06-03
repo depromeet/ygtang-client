e881d7f9d6976594822614c882520e277de2866a
Hyesung Oh <haesungoh414@gmail.com>
2022-06-03 15:13:08 +0900

    refactor: chang-nickname route 관심사 분리

--- src/pages/my/account/change-nickname.tsx
+++ src/pages/my/account/change-nickname.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 import { css } from '@emotion/react';
 
 import useUserInformationMutation from '~/hooks/api/member/useUserInformationMutaion';
@@ -13,52 +13,13 @@ import NavigationBar from '../../../components/common/NavigationBar';
 import TextField from '../../../components/common/TextField';
 
 export default function MyAccountChangeNickame() {
-  const { fireToast } = useToast();
   const nickname = useInput({ useDebounce: true });
-  const { userInformation } = useUserInformation();
   const [nicknameError, setNicknameError] = useState('');
-  const { updateNickname } = useUserInformationMutation();
-  const { push } = useInternalRouter();
-
-  useDidUpdate(() => {
-    nickname.setValue(userInformation.nickName);
-  }, [userInformation.nickName]);
-
-  const isNicknameNotValidateForLength =
-    nickname.debouncedValue.length < 4 || 20 < nickname.debouncedValue.length;
-
-  const isNicknameSameWithPrev = userInformation.nickName === nickname.debouncedValue;
-
-  useDidUpdate(() => {
-    if (isNicknameNotValidateForLength) {
-      setNicknameError('닉네임은 4자 이상 20자 이하여야 합니다.');
-      return;
-    }
-    if (isNicknameSameWithPrev) {
-      setNicknameError('변경될 이름을 입력해주세요.');
-      return;
-    }
-
-    setNicknameError('');
-  }, [nickname.debouncedValue]);
-
-  const onFormReturn = async (e: React.FormEvent) => {
-    e.preventDefault();
-    callMuation();
-  };
-
-  const callMuation = () => {
-    if (nicknameError) return fireToast({ content: nicknameError });
-
-    updateNickname(
-      { nickname: nickname.debouncedValue },
-      {
-        onSuccess: () => {
-          push('/my/account');
-        },
-      }
-    );
-  };
+  const { callMuation, onFormReturn, isValidateNickname } = useChangeNickname({
+    nickname,
+    nicknameError,
+    setNicknameError,
+  });
 
   return (
     <article css={editNickNameCss}>
@@ -80,7 +41,7 @@ export default function MyAccountChangeNickame() {
         <TextField
           label="이름"
           feedback={nicknameError}
-          isSuccess={!isNicknameNotValidateForLength && !isNicknameSameWithPrev}
+          isSuccess={isValidateNickname}
           value={nickname.value}
           onChange={nickname.onChange}
           required
@@ -94,6 +55,69 @@ const editNickNameCss = css`
   display: flex;
   flex-direction: column;
 `;
+
 const formCss = css`
   margin-top: 32px;
 `;
+
+interface UseChangeNicknameProps {
+  nickname: ReturnType<typeof useInput>;
+  nicknameError: string;
+  setNicknameError: Dispatch<SetStateAction<string>>;
+}
+
+function useChangeNickname({ nickname, nicknameError, setNicknameError }: UseChangeNicknameProps) {
+  const { fireToast } = useToast();
+  const { userInformation } = useUserInformation();
+  const { updateNickname } = useUserInformationMutation();
+  const { push } = useInternalRouter();
+
+  // NOTE: 변경 전 닉네임 setStating
+  useDidUpdate(() => {
+    nickname.setValue(userInformation.nickName);
+  }, [userInformation.nickName]);
+
+  const isNicknameNotValidateForLength =
+    nickname.debouncedValue.length < 4 || 20 < nickname.debouncedValue.length;
+
+  const isNicknameSameWithPrev = userInformation.nickName === nickname.debouncedValue;
+
+  const isValidateNickname = !isNicknameNotValidateForLength && !isNicknameSameWithPrev;
+
+  useDidUpdate(() => {
+    if (isNicknameNotValidateForLength) {
+      setNicknameError('닉네임은 4자 이상 20자 이하여야 합니다.');
+      return;
+    }
+    if (isNicknameSameWithPrev) {
+      setNicknameError('변경될 이름을 입력해주세요.');
+      return;
+    }
+
+    setNicknameError('');
+  }, [nickname.debouncedValue]);
+
+  const callMuation = () => {
+    if (nicknameError) return fireToast({ content: nicknameError });
+
+    updateNickname(
+      { nickname: nickname.debouncedValue },
+      {
+        onSuccess: () => {
+          push('/my/account');
+        },
+      }
+    );
+  };
+
+  const onFormReturn = async (e: React.FormEvent) => {
+    e.preventDefault();
+    callMuation();
+  };
+
+  return {
+    onFormReturn,
+    callMuation,
+    isValidateNickname,
+  };
+}
