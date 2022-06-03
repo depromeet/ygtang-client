a0907b3ea287fbe87bdcabcfcb3bf7ccb4cdb466
Hyesung Oh <haesungoh414@gmail.com>
2022-06-03 15:22:22 +0900

    test: Button 컴포넌트 onClick 테스트 추가

--- src/components/common/Button/Button.test.tsx
+++ src/components/common/Button/Button.test.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import Button from './Button';
 
 const buttonText = '버튼 내용';
 
 describe('components/common/Button/Button', () => {
+  it('should defined', () => {
+    expect(Button).toBeDefined();
+  });
+
   it('should render children', () => {
     render(<Button>{buttonText}</Button>);
     expect(screen.getByText(buttonText)).toBeInTheDocument();
+  });
+
+  it('onClick props가 정상적으로 동작해야함', () => {
+    const onClickMock = jest.fn();
+    render(<Button onClick={onClickMock}>{buttonText}</Button>);
+
+    expect(onClickMock).not.toBeCalled();
+    fireEvent.click(screen.getByText(buttonText));
+    expect(onClickMock).toBeCalled();
+    expect(onClickMock).toBeCalledTimes(1);
   });
 });
