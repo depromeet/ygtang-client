42f54f5bfbee654d85e1c16808081db506af7c91
positiveko <edie.spark1er@gmail.com>
2022-06-04 18:31:37 +0900

    feat: layout 모듈호 및 webview safe-area 적용

---
+++ src/pages/_layout.tsx
@@ -0,0 +1,44 @@
+import { PropsWithChildren, useEffect } from 'react';
+import { css, Theme } from '@emotion/react';
+
+import { useWindowSize } from '~/hooks/common/useWindowSize';
+import { fullViewHeight } from '~/styles/utils';
+
+let vh = 0;
+
+export default function Layout({ children }: PropsWithChildren<{}>) {
+  const windowSize = useWindowSize();
+
+  useEffect(() => {
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, [windowSize.height]);
+
+  return (
+    <div css={layoutCss}>
+      <div css={safeAreaTopCss} />
+      {children}
+      <div css={safeAreaBottomCss} />
+    </div>
+  );
+}
+
+const layoutCss = (theme: Theme) => css`
+  min-height: ${fullViewHeight()};
+  background: ${theme.color.background};
+  max-width: ${theme.size.maxWidth};
+  width: 100%;
+  margin: 0 auto;
+  padding: ${theme.size.layoutPadding};
+`;
+
+const safeAreaTopCss = css`
+  height: calc(constant(safe-area-inset-top));
+  height: calc(env(safe-area-inset-top));
+  background-color: red;
+`;
+
+const safeAreaBottomCss = css`
+  height: calc(constant(safe-area-inset-bottom));
+  height: calc(env(safe-area-inset-bottom));
+`;
