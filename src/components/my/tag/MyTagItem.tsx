b18db61b69902f8ca89b0f3ae42c1204c094d3dc
정대윤 <59507527+SenseCodeValue@users.noreply.github.com>
2022-06-04 16:17:27 +0900

    fix: 태그관리 태그 삭제 모달 추가 (#346)

--- src/components/my/tag/MyTagItem.tsx
+++ src/components/my/tag/MyTagItem.tsx
@@ -1,4 +1,8 @@
-import { IconButton } from '~/components/common/Button';
+import { useState } from 'react';
+import { css } from '@emotion/react';
+
+import { FilledButton, IconButton } from '~/components/common/Button';
+import Dialog from '~/components/common/Dialog';
 import Menu from '~/components/my/Menu';
 import useTagMutation from '~/hooks/api/tag/useTagMutation';
 import { useToast } from '~/store/Toast';
@@ -6,19 +10,54 @@ import { useToast } from '~/store/Toast';
 export default function MyTagItem({ tag }: { tag: TagType }) {
   const { deleteTag } = useTagMutation();
   const { fireToast } = useToast();
+
+  const [isTagDeleteConfirmModalOpen, setisTagDeleteConfirmModalOpen] = useState(false);
+
   const onDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation();
-    deleteTag(tag.id, {
-      onSuccess: () => {
-        fireToast({ content: '태그 삭제 성공!' });
-      },
-    });
+    setisTagDeleteConfirmModalOpen(true);
   };
 
   return (
-    <Menu
-      label={tag.content}
-      rightElement={<IconButton onClick={onDelete} iconName="RemoveCircleIcon" light />}
-    />
+    <>
+      <Menu
+        label={tag.content}
+        rightElement={<IconButton onClick={onDelete} iconName="RemoveCircleIcon" light />}
+      />
+      <Dialog
+        isShowing={isTagDeleteConfirmModalOpen}
+        actionButtons={
+          <>
+            <FilledButton
+              colorType="dark"
+              onClick={() => {
+                deleteTag(tag.id, {
+                  onSuccess: () => {
+                    fireToast({ content: '태그 삭제했습니다.' });
+                    setisTagDeleteConfirmModalOpen(false);
+                  },
+                });
+              }}
+            >
+              네
+            </FilledButton>
+            <div css={dialogLongButtonCss}>
+              <FilledButton colorType="light" onClick={() => setisTagDeleteConfirmModalOpen(false)}>
+                아니요
+              </FilledButton>
+            </div>
+          </>
+        }
+      >
+        선택한 태그가 모든 영감에서
+        <br />
+        사라집니다! 괜찮으신가요?
+      </Dialog>
+    </>
   );
 }
+
+const dialogLongButtonCss = css`
+  width: 163px;
+  flex-shrink: 0;
+`;
