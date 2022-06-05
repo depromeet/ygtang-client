0ad38c257f65c725ce10d4b9d64c8892582343ee
Hyesung Oh <haesungoh414@gmail.com>
2022-06-06 00:42:12 +0900

    feat: open graph 이미지 대응 훅 개발

---
+++ src/hooks/common/useOpenGraphImage/useOpenGraphImage.ts
@@ -0,0 +1,33 @@
+import { SyntheticEvent, useEffect, useState } from 'react';
+
+interface UseOpenGraphImageProps {
+  url: string | undefined;
+  image: string | undefined;
+}
+
+export default function useOpenGraphImage({ url, image }: UseOpenGraphImageProps) {
+  const [src, setSrc] = useState<string>('');
+
+  useEffect(() => {
+    // NOTE: initial state를 og.url + og.image로 한 이유는
+    // og.image가 안되는 경우가 상대주소로 작성이 되어 있어 영감탱 서버에 요청하기 때문
+    if (url && image) {
+      setSrc(url + image);
+    } else if (image) {
+      setSrc(image);
+    } else if (url) {
+      setSrc(url);
+    }
+  }, [image, url]);
+
+  const onImageError = (e: SyntheticEvent<HTMLImageElement>) => {
+    if (!url) return;
+    if (!image) return;
+
+    // NOTE: 두번째로 시도하는 og.image에서도 에러를 발생할 경우
+    if (e.currentTarget.src === image) return;
+    setSrc(image);
+  };
+
+  return { src, onImageError };
+}
