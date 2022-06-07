29c69bad2950562c7070b1efc0db2829cd981b70
Hyesung Oh <haesungoh414@gmail.com>
2022-06-08 01:39:41 +0900

    test: 로그인 성공 시 e2e 테스트 작성

---
+++ cypress/integration/3_login/login_success.spec.ts
@@ -0,0 +1,32 @@
+describe('3_login/login success', () => {
+  it('이메일란에 올바른 이메일 입력시 확인 아이콘이 보여야 합니다', () => {
+    cy.visit('/login');
+
+    cy.get('label')
+      .contains('이메일 아이디')
+      .parent()
+      .within(() => {
+        cy.get('input').first().click().type(Cypress.env('user-email'));
+
+        cy.get('svg').should('be.visible');
+      });
+  });
+
+  it('비밀번호란에 올바른 비밀번호 입력시 확인 아이콘이 보여야 합니다', () => {
+    cy.get('label')
+      .contains('비밀번호')
+      .parent()
+      .within(() => {
+        cy.get('input').first().click().type(Cypress.env('user-password'));
+
+        cy.get('svg').should('be.visible');
+      });
+  });
+
+  it('로그인 시 루트 url로 이동되어야 합니다', () => {
+    cy.get('button').should('have.attr', 'type', 'submit').contains('로그인').click();
+    cy.wait(2000).url().should('eq', 'https://localhost:3000/');
+  });
+});
+
+export {};
