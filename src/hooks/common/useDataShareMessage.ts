ee2bd4bc3ae50de231bab147f2c510394b44a8b3
정대윤 <sensecodevalue@gmail.com>
2022-06-14 23:24:35 +0900

    remove: console.log

--- src/hooks/common/useDataShareMessage.ts
+++ src/hooks/common/useDataShareMessage.ts
@@ -21,7 +21,6 @@ export function useDataShareMessage(setStateHandler: (data: string) => void) {
   // NOTE : document.addEventListener('message', handleMessage)에 event type 중 MessageEvent 존재하지 않아 선대처합니다.
   const handleMessage = (event: MessageEvent | any) => {
     const data = JSON.parse(event.data);
-    console.log(data);
     if (data.type !== SHARE_EXTENTION_MESSAGE_TYPE) return;
     setStateHandler(data.data);
   };
