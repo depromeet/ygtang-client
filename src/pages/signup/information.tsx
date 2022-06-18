a25de0aade3d70bd8494743e9a732773e02dd78d
Hyesung Oh <haesungoh414@gmail.com>
2022-06-18 16:01:40 +0900

    feat: 회원가입 시 router replace 변경

--- src/pages/signup/information.tsx
+++ src/pages/signup/information.tsx
@@ -85,7 +85,7 @@ export default function Information() {
             {
               onSuccess: () => {
                 clearSignupUser();
-                router.push('/');
+                router.replace('/');
               },
             }
           );
