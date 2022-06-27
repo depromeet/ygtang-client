58fddf1f20a0c5a1e43ec8918d63f69f80cd27ea
doda <ddark.kr@gmail.com>
2022-06-27 20:41:44 +0900

    feat: appendButton bottom 위치 대응

--- src/components/home/AppendButton.tsx
+++ src/components/home/AppendButton.tsx
@@ -43,7 +43,7 @@ export default function AppendButton() {
 const buttonCss = (theme: Theme) => css`
   position: fixed;
   right: 16px;
-  bottom: 24px;
+  bottom: 70px;
 
   display: flex;
   justify-content: center;
