19d941a302c925d26e65b673ab13b7f4bb991327
doda <ddark.kr@gmail.com>
2022-06-28 19:25:37 +0900

    refactor: remove duplicate check

--- src/hooks/common/useWebViewMessage.ts
+++ src/hooks/common/useWebViewMessage.ts
@@ -1,10 +1,10 @@
 import { useUserAgent } from '~/hooks/common/useUserAgent';
 
 export function useWebViewMessage() {
-  const { isAndroid, isIos, isMobile } = useUserAgent();
+  const { isMobile } = useUserAgent();
 
   const postMessage = (type: string, data?: any) => {
-    if (!isAndroid() && !isIos() && !isMobile()) return;
+    if (!isMobile()) return;
     if (!window.ReactNativeWebView) return;
     window.ReactNativeWebView.postMessage(JSON.stringify({ type, data }));
   };
