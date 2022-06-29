0d1e2b1cb77d1570974deeefda9c789b03788d98
doda <ddark.kr@gmail.com>
2022-06-29 15:13:42 +0900

    chore: 테스트

--- src/pages/add/text.tsx
+++ src/pages/add/text.tsx
@@ -34,7 +34,6 @@ export default function AddText() {
 
   const onMutationError = () => {
     fireToast({ content: '오류가 발생했습니다. 다시 시도해주세요.', duration: 3500 });
-    postMessage(WEBVIEW_MESSAGE_TYPE.ClosedInspiration);
   };
 
   const { createInspiration, isCreateInspirationLoading } = useInspirationMutation({
