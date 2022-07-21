0b6c32332da00b81440854b25deeddb379e46b2f
Hyesung Oh <haesungoh414@gmail.com>
2022-07-22 02:22:50 +0900

    fix: 이미지 다운로드시 DOM 객체 생성

--- src/utils/common/imageDownload.ts
+++ src/utils/common/imageDownload.ts
@@ -7,13 +7,20 @@ export async function imageDownload({ href }: Props) {
   const imageBlob = await imageResponse.blob();
   const imageObjectUrl = window.URL.createObjectURL(imageBlob);
 
+  console.log(imageObjectUrl);
+
   const element = document.createElement('a');
   element.href = imageObjectUrl;
 
   const imageName = new Date().getTime();
-  const imageExtension = href.split('.').at(-1); // 업로드시와 동일한 확장자
+  const imageExtension = href.split('.').at(-1);
   element.download = `${imageName}.${imageExtension}`;
-  element.click();
+
+  document.body.appendChild(element);
+
+  element.dispatchEvent(new MouseEvent('click'));
+
+  document.body.removeChild(element);
 
   window.URL.revokeObjectURL(imageObjectUrl);
 }
