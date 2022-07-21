cdcd3bbdc273c5215d7b98908e909ab28af0e397
Hyesung Oh <haesungoh414@gmail.com>
2022-07-22 03:16:57 +0900

    fix: 이미지 다운로드 로직 file saver 사용

--- src/utils/common/imageDownload.ts
+++ src/utils/common/imageDownload.ts
@@ -1,3 +1,5 @@
+import { saveAs } from 'file-saver';
+
 interface Props {
   href: string;
 }
@@ -7,17 +9,25 @@ export async function imageDownload({ href }: Props) {
   const imageBlob = await imageResponse.blob();
   const imageObjectUrl = window.URL.createObjectURL(imageBlob);
 
-  const element = document.createElement('a');
-  element.href = imageObjectUrl;
-
   const imageName = new Date().getTime();
   const imageExtension = href.split('.').at(-1);
 
-  element.download = `${imageName}.${imageExtension}`;
+  saveAs(imageObjectUrl, `${imageName}.${imageExtension}`);
 
-  document.body.appendChild(element);
-  element.dispatchEvent(new MouseEvent('click'));
-  document.body.removeChild(element);
+  // const imageResponse = await fetch(href);
+  // const imageBlob = await imageResponse.blob();
+  // const imageObjectUrl = window.URL.createObjectURL(imageBlob);
 
-  window.URL.revokeObjectURL(imageObjectUrl);
+  // const element = document.createElement('a');
+  // element.href = imageObjectUrl;
+
+  // const imageName = new Date().getTime();
+  // const imageExtension = href.split('.').at(-1);
+  // element.download = `${imageName}.${imageExtension}`;
+
+  // document.body.appendChild(element);
+  // element.dispatchEvent(new MouseEvent('click'));
+  // document.body.removeChild(element);
+
+  // window.URL.revokeObjectURL(imageObjectUrl);
 }
