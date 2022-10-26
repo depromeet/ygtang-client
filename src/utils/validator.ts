3fc07b23f97a901af80d81b82f221c507edc4d7a
도다 <ddark.kr@gmail.com>
2022-10-26 16:54:12 +0900

    fix: url surfix의 인식 범위를 2~9으로 늘림

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -20,7 +20,7 @@ interface Validator {
 const pattern: Record<ValidatorType, RegExp> = {
   email:
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+\.)+[a-zA-Z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]{2,}))$/,
-  url: /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&/=]*)/g,
+  url: /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,9}\b([-a-zA-Z0-9@:%_\+.~#?&/=]*)/g,
 };
 
 export const validator = ({ value, type, rule }: Validator) => {
