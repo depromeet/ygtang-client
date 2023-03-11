dc0dee0806e70135de6742799bdc59b07d581687
도다 <ddark.kr@gmail.com>
2023-03-11 22:35:27 +0900

    ci: cypress test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'cypress';
+const { defineConfig } = require('cypress');
 
-export default defineConfig({
+module.exports = defineConfig({
   projectId: 'tmhk21',
   viewportWidth: 414,
   viewportHeight: 896,
