8f9605769c2444ac21f1d419486351bdc30dfe53
hyesung oh <haesungoh414@gmail.com>
2023-03-11 19:17:36 +0900

    필터링 이벤트 부착 (#534)
    * feat: nav filter event 수집
    
    * feat: 필터링 바텀 시트 이벤트 수집

--- src/components/home/FilterBottomSheetModal/TagFilterSection.tsx
+++ src/components/home/FilterBottomSheetModal/TagFilterSection.tsx
@@ -4,13 +4,25 @@ import { css, Theme } from '@emotion/react';
 import { ChevronIcon } from '~/components/common/icons';
 import AppliedTags from '~/components/common/TagForm/AppliedTags';
 import { useFilteredTags } from '~/store/FilteredTags';
+import { recordEvent } from '~/utils/analytics';
 
 export default function TagFilterSection() {
   const { filteredTags, removeTag } = useFilteredTags({});
 
+  const onClickTagLink = () => {
+    recordEvent({ action: '태그 필터 클릭' });
+  };
+
   return (
     <section css={sectionCss}>
-      <Link href="/?modal=tag" as="/tag" scroll={false} passHref css={tagFilterAnchorCss}>
+      <Link
+        href="/?modal=tag"
+        as="/tag"
+        scroll={false}
+        passHref
+        css={tagFilterAnchorCss}
+        onClick={onClickTagLink}
+      >
         태그 <ChevronIcon direction="right" />
       </Link>
       {Boolean(filteredTags.length) && (
--- src/components/home/HomeNavigationBar.tsx
+++ src/components/home/HomeNavigationBar.tsx
@@ -1,18 +1,20 @@
 import { css, Theme } from '@emotion/react';
 
 import useToggle from '~/hooks/common/useToggle';
+import { recordEvent } from '~/utils/analytics';
 
 import { FilterIcon, SettingsIcon } from '../common/icons';
 import InternalLink from '../common/InternalLink';
 import FilterBottomSheetModal from './FilterBottomSheetModal';
 
 export default function HomeNavigationBar() {
-  const [isFilterBottomSheetModalOpen, toggleFilterBottomSheetModal] = useToggle(false);
+  const { isFilterBottomSheetModalOpen, toggleFilterBottomSheetModal, onFilterClick } =
+    useToggleFilterBottomSheet();
 
   return (
     <>
       <nav css={navCss}>
-        <button onClick={toggleFilterBottomSheetModal}>
+        <button onClick={onFilterClick}>
           <FilterIcon css={iconCss} />
         </button>
         <InternalLink href="/my">
@@ -47,3 +49,14 @@ const iconCss = (theme: Theme) => css`
   margin: 10px;
   color: ${theme.color.gray05};
 `;
+
+function useToggleFilterBottomSheet() {
+  const [isFilterBottomSheetModalOpen, toggleFilterBottomSheetModal] = useToggle(false);
+
+  function onFilterClick() {
+    toggleFilterBottomSheetModal();
+    recordEvent({ action: 'nav filter 아이콘 클릭' });
+  }
+
+  return { isFilterBottomSheetModalOpen, toggleFilterBottomSheetModal, onFilterClick };
+}
