7a95e45e5df4a438d206b1f3a2f8b179b2542e11
Hyesung Oh <haesungoh414@gmail.com>
2023-03-12 17:13:14 +0900

    chore: 이벤트 기록 시 package version 참조

--- src/utils/analytics/event.ts
+++ src/utils/analytics/event.ts
@@ -1,9 +1,13 @@
+import { version } from '~/../package.json';
 import { IS_PRODUCTION } from '~/constants/common';
 import { gaEvent } from '~/libs/ga';
 import { mixpanelTrack } from '~/libs/mixpanel';
 
-export function recordEvent({ action, category, label, value }: Parameters<typeof gaEvent>[0]) {
+type GaEventProps = Parameters<typeof gaEvent>[0];
+type Props = Omit<GaEventProps, 'category'>;
+
+export function recordEvent({ action, label, value }: Props) {
   if (!IS_PRODUCTION) return;
-  gaEvent({ action, category, label, value });
-  mixpanelTrack(action, { category, label, value });
+  gaEvent({ action, category: version, label, value });
+  mixpanelTrack(action, { category: version, label, value });
 }
