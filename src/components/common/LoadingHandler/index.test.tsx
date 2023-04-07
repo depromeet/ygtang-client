7ba107b58d80b0a7f0c2c19998b4b15e16207278
Hyesung Oh <haesungoh414@gmail.com>
2023-04-07 17:18:48 +0900

    chore: rename describe

--- src/components/common/LoadingHandler/index.test.tsx
+++ src/components/common/LoadingHandler/index.test.tsx
@@ -1,6 +1,6 @@
 import LoadingHandler from './index';
 
-describe('components/common/LoadingHandler', () => {
+describe('components/common/LoadingHandler/index', () => {
   it('should defined', () => {
     expect(LoadingHandler).toBeDefined();
   });
