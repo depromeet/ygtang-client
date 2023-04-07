87f1cb055221dc92dfbc6a9e11d54cc2db788505
hyesung oh <haesungoh414@gmail.com>
2023-04-08 02:04:35 +0900

    loading handler 테스트 (#559)
    * test: index
    
    * chore: rename describe
    
    * test: loading handler

---
+++ src/components/common/LoadingHandler/index.test.tsx
@@ -0,0 +1,7 @@
+import LoadingHandler from './index';
+
+describe('components/common/LoadingHandler/index', () => {
+  it('should defined', () => {
+    expect(LoadingHandler).toBeDefined();
+  });
+});
